31f04dd75f03d27b91393dc099455be5acc9d80f
Birk Johansson <birk@dhis2.org>
2021-12-13 16:19:33 +0100

    wip: calculate nr of columns

--- src/data-workspace/data-table.js
+++ src/data-workspace/data-table.js
@@ -38,26 +38,41 @@ export const DataTable = ({ metadata, dataSetId }) => {
     console.log(catoptcombos)
 
     //return 'Hello'
+    let catColSpan = catoptcombos.length
 
+    const colRepeat = categories.reduce((acc, cat) => {
+        const nrOfOptions = cat.categoryOptions.length
+
+        if (nrOfOptions > 0 && catColSpan >= nrOfOptions) {
+            catColSpan = catColSpan / nrOfOptions
+            const total = catoptcombos.length / (catColSpan * nrOfOptions)
+            acc[cat.id] = total
+        }
+        return acc
+    }, {})
+    console.log(colRepeat)
     return (
         <Table>
             <TableHead>
-                {categories.map((c) => (
-                    <TableRowHead>
-                        {catoptcombos.map((coc) => {
-                            const cocOptions = getCategoryOptionsByCoCId(
-                                metadata,
-                                coc.id
-                            )
-                            //console.log({ cocOptions })
-                            return (
-                                <TableCellHead>
-                                    {cocOptions[0].displayFormName}
-                                </TableCellHead>
-                            )
-                        })}
-                    </TableRowHead>
-                ))}
+                {categories.map((c) => {
+                    //const totalColumns =
+                    return (
+                        <TableRowHead>
+                            {catoptcombos.map((coc) => {
+                                const cocOptions = getCategoryOptionsByCoCId(
+                                    metadata,
+                                    coc.id
+                                )
+                                //console.log({ cocOptions })
+                                return (
+                                    <TableCellHead colSpan={1}>
+                                        {cocOptions[0].displayFormName}
+                                    </TableCellHead>
+                                )
+                            })}
+                        </TableRowHead>
+                    )
+                })}
             </TableHead>
         </Table>
     )
