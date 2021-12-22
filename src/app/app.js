3522b26871b23e4b0115fa20349eb4e7a79aab53
ismay <7355199+ismay@users.noreply.github.com>
2021-12-22 15:35:54 +0100

    chore(react-query): add basic structure for react-query

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,30 @@
+import { useDataEngine } from '@dhis2/app-runtime'
 import { CssVariables } from '@dhis2/ui'
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './app.css'
 import { Layout } from './layout/index.js'
 
 const App = () => {
+    const engine = useDataEngine()
+
+    // https://react-query.tanstack.com/guides/query-keys
+    const queryFn = ({ queryKey }) => {
+        const [query, variables] = queryKey
+        return engine.query(query, { variables })
+    }
+
+    // https://react-query.tanstack.com/guides/default-query-function
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                queryFn,
+            },
+        },
+    })
+
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <CssVariables colors />
             <Layout
                 header=""
@@ -15,7 +34,7 @@ const App = () => {
                 showSidebar
                 showFooter
             />
-        </>
+        </QueryClientProvider>
     )
 }
 
