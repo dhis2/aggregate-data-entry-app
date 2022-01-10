e3ae9c4f2e4e3c1df06ea98f48bdd32fafe47163
Birk Johansson <birk@dhis2.org>
2022-01-10 20:39:00 +0100

    fix: section header styling

--- src/data-workspace/section.js
+++ src/data-workspace/section.js
@@ -1,5 +1,12 @@
 import i18n from '@dhis2/d2-i18n'
-import { colors, IconFilter16, Table } from '@dhis2/ui'
+import {
+    colors,
+    IconFilter16,
+    Table,
+    TableCellHead,
+    TableHead,
+    TableRowHead,
+} from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React, { useContext, useState } from 'react'
 import { CategoryComboTable } from './category-combo-table.js'
@@ -38,88 +45,52 @@ export const FormSection = ({ section, getDataValue, globalFilterText }) => {
     )
 
     return (
-        <section className="wrapper">
-            <header className="header">
-                <div className="title">{section.displayName}</div>
-                {section.description && (
-                    <div className="description">
-                        {section.description ||
-                            'Placeholder section description'}
-                    </div>
-                )}
-            </header>
-            <div className="filter">
-                <IconFilter16 color={colors.grey600} />
-                <input
-                    name="filter-input"
-                    type="text"
-                    placeholder={i18n.t('Type here to filter in this section')}
-                    value={filterText}
-                    onChange={({ target }) => setFilterText(target.value)}
-                />
-            </div>
-            <div>
-                <Table className={styles.table}>
-                    {groupedDataElements.map(
-                        ({ categoryCombo, dataElements }) => (
-                            <CategoryComboTable
-                                key={categoryCombo.id}
-                                categoryCombo={categoryCombo}
-                                dataElements={dataElements}
-                                getDataValue={getDataValue}
-                                filterText={filterText}
-                                globalFilterText={globalFilterText}
-                                maxColumnsInSection={maxColumnsInSection}
+        <div className="wrapper">
+            <Table className={styles.table}>
+                <TableHead>
+                    <TableRowHead>
+                        <TableCellHead colSpan="100%" className={styles.header}>
+                            <div className={styles.title}>
+                                {section.displayName}
+                            </div>
+                            {section.description && (
+                                <div className={styles.description}>
+                                    {section.description ||
+                                        'Placeholder section description'}
+                                </div>
+                            )}
+                        </TableCellHead>
+                    </TableRowHead>
+                    <TableRowHead>
+                        <TableCellHead colSpan="100%" className={styles.filter}>
+                            <IconFilter16 color={colors.grey600} />
+                            <input
+                                name="filter-input"
+                                type="text"
+                                placeholder={i18n.t(
+                                    'Type here to filter in this section'
+                                )}
+                                value={filterText}
+                                onChange={({ target }) =>
+                                    setFilterText(target.value)
+                                }
                             />
-                        )
-                    )}
-                </Table>
-            </div>
-
-            {/* Todo: verify styles with joe - 
-            line height for title & description, lack of focus styles on input,
-            inset box shadow in title?, grey300 on section description */}
-            <style jsx>
-                {`
-                    header {
-                        background-color: ${colors.grey800};
-                        line-height: 20px;
-                        padding: 4px 8px;
-                    }
-                    .title {
-                        color: ${colors.grey050};
-                        font-weight: 500;
-                        font-size: 14px;
-                    }
-                    .description {
-                        color: ${colors.grey300};
-                        font-size: 13px;
-                        margin-top: 2px;
-                    }
-                    .filter {
-                        display: flex;
-                        align-items: center;
-                        background-color: #fff;
-                        font-size: 13px;
-                        line-height: 15px;
-                        padding: 8px;
-                        gap: 8px;
-                    }
-                    .filter input {
-                        width: 100%;
-                        background: none;
-                        border: none;
-                        color: ${colors.grey900};
-                    }
-                    .filter input::placeholder {
-                        color: ${colors.grey600};
-                    }
-                    .filter input:focus {
-                        outline: none;
-                    }
-                `}
-            </style>
-        </section>
+                        </TableCellHead>
+                    </TableRowHead>
+                </TableHead>
+                {groupedDataElements.map(({ categoryCombo, dataElements }) => (
+                    <CategoryComboTable
+                        key={categoryCombo.id}
+                        categoryCombo={categoryCombo}
+                        dataElements={dataElements}
+                        getDataValue={getDataValue}
+                        filterText={filterText}
+                        globalFilterText={globalFilterText}
+                        maxColumnsInSection={maxColumnsInSection}
+                    />
+                ))}
+            </Table>
+        </div>
     )
 }
 FormSection.propTypes = {
