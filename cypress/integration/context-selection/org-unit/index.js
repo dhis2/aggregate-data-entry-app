80ac73b93a76389d6af8746a4bccbba8e4123f0c
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-01-17 16:30:45 +0100

    test(org unit selector): add feature file and empty test steps

---
+++ cypress/integration/context-selection/org-unit/index.js
@@ -0,0 +1,53 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+
+Given('a data set and an org unit have been selected', () => {})
+
+Given('a data set and no org unit have been selected yet', () => {})
+
+Given('no data set has been selected', () => {})
+
+Given('the user has selected a data set', () => {})
+
+When('loading the org unit fails', () => {})
+
+When('loading the org unit is done', () => {})
+
+When('selects an org unit', () => {})
+
+When('the root org units have been loaded', () => {})
+
+When('the user closes the org unit selector', () => {})
+
+When('the user filters the org units by the name of a non-root org unit', () => {})
+
+When('the user opens expands the root org unit', () => {})
+
+When('the user opens the org unit selector', () => {})
+
+When('the user selects a different data set connected to the currently selected org unit', () => {})
+
+When('the user selects a different data set not connected to the currently selected org unit', () => {})
+
+Then('a loading message should be displayed while loading its data', () => {})
+
+Then('a no-value-label should be displayed', () => {})
+
+Then('a tooltip with the content "Choose a data set first" should be shown', () => {})
+
+Then('an error message should be displayed', () => {})
+
+Then('its value-label should be displayed', () => {})
+
+Then('the org unit should be deselected', () => {})
+
+Then('the org unit should remain selected', () => {})
+
+Then('the org unit tree should be hidden', () => {})
+
+Then('the org unit tree should only display nodes up until the filtered org unit', () => {})
+
+Then("the org unit's display name should be displayed as selected value", () => {})
+
+Then("the org unit's id should be persisted", () => {})
+
+Then("the root org unit's children should be displayed", () => {})
