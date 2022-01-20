de5f1a262a4c5b5b2bbe429e60ea258e7ab283d9
Kai Vandivier <kpvandivier@gmail.com>
2022-01-20 13:14:03 +0100

    refactor(use-active-cell): remove unused coIds

--- src/data-workspace/data-entry-cell/use-active-cell.js
+++ src/data-workspace/data-entry-cell/use-active-cell.js
@@ -1,19 +1,18 @@
 import { useFormState } from 'react-final-form'
-import { useMetadata } from '../metadata-context.js'
 
 export const useActiveCell = () => {
     const { active } = useFormState({ subscription: { active: true } })
-    const { metadata } = useMetadata()
 
     const [deId, cocId] = active ? active.split('.') : [null, null]
-    const coIds = active
-        ? metadata.categoryOptionCombos[cocId].categoryOptions
-        : []
+
+    // Optional category option IDs (requires useMetadata hook):
+    // const coIds = active
+    //     ? getCategoryOptionComboById(metadata, cocId).categoryOptions
+    //     : []
 
     return {
         active,
         deId,
         cocId,
-        coIds,
     }
 }
