e13d3e483e7613431c9a0836fd80b41bc784e27d
Kai Vandivier <kpvandivier@gmail.com>
2022-01-24 22:19:12 +0100

    feat(de-tooltip): open and close on focus and blur

--- src/data-workspace/data-entry-cell/validation-tooltip.js
+++ src/data-workspace/data-entry-cell/validation-tooltip.js
@@ -5,48 +5,54 @@ import React from 'react'
 /**
  * These components adapt the UI Tooltip component to be used for the
  * validation feedback tooltip in DE cells, which has a few needs:
- * 1. Should only open when the cell is invalid
+ * 1. Should only open when the cell state is invalid
  * 2. Should gracefully close when the cell becomes valid again
  * 3. Should not cause cell to lose focus when it becomes invalid
+ * 4. Should open when focused and close when defocused
  */
 
 const TooltipManager = React.forwardRef(function TooltipManager(
-    { children, invalid, onMouseOut, onMouseOver },
+    { active, children, invalid, onMouseOut: close, onMouseOver: open },
     ref
 ) {
     React.useEffect(() => {
-        // When this cell becomes valid, close the tooltip
-        if (!invalid) {
-            onMouseOut()
+        if (invalid && active) {
+            open()
         }
-    }, [invalid])
+        // When this cell becomes valid or defocused, close the tooltip
+        else {
+            close()
+        }
+    }, [invalid, active])
 
     // Only open the tooltip if the cell is invalid
-    const handleMouseOver = () => {
+    const onMouseOver = () => {
         if (invalid) {
-            onMouseOver()
+            open()
         }
     }
-    const handleMouseOut = () => {
-        if (invalid) {
-            onMouseOut()
+    // Close on mouseout if invalid cell is not active
+    const onMouseOut = () => {
+        if (invalid && !active) {
+            close()
         }
     }
 
     return children({
-        onMouseOver: handleMouseOver,
-        onMouseOut: handleMouseOut,
+        onMouseOver,
+        onMouseOut,
         ref,
     })
 })
 TooltipManager.propTypes = {
+    active: PropTypes.bool,
     children: PropTypes.func,
     invalid: PropTypes.bool,
     onMouseOut: PropTypes.func,
     onMouseOver: PropTypes.func,
 }
 
-export const ValidationTooltip = ({ invalid, error, children }) => {
+export const ValidationTooltip = ({ active, children, error, invalid }) => {
     const [content, setContent] = React.useState(error)
 
     // Keep tooltip content even when `error` is undefined so tooltip still has
@@ -60,7 +66,7 @@ export const ValidationTooltip = ({ invalid, error, children }) => {
     return (
         <Tooltip content={content}>
             {(props) => (
-                <TooltipManager {...props} invalid={invalid}>
+                <TooltipManager {...props} invalid={invalid} active={active}>
                     {children}
                 </TooltipManager>
             )}
@@ -68,6 +74,7 @@ export const ValidationTooltip = ({ invalid, error, children }) => {
     )
 }
 ValidationTooltip.propTypes = {
+    active: PropTypes.bool,
     children: PropTypes.func,
     error: PropTypes.string,
     invalid: PropTypes.bool,
