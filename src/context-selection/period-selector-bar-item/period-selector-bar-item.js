a8394b76b0dd7cae6645e3c83cd51f9ec5866f7f
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-01-26 14:32:09 +0100

    feat(period selector): add tooltip when disabled

--- src/context-selection/period-selector-bar-item/period-selector-bar-item.js
+++ src/context-selection/period-selector-bar-item/period-selector-bar-item.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { usePeriod } from '../../shared/index.js'
 import { useDataSetId, usePeriodId } from '../use-context-selection/index.js'
 import computeMaxYear from './compute-max-year.js'
+import DisabledTooltip from './disabled-tooltip.js'
 import PeriodMenu from './period-menu.js'
 import useDataSetPeriodType from './use-data-set-period-type.js'
 import useDeselectOnPeriodTypeChange from './use-deselect-on-period-type-change.js'
@@ -47,37 +48,41 @@ export const PeriodSelectorBarItem = () => {
     useDeselectOnPeriodTypeChange()
 
     return (
-        <SelectorBarItem
-            disabled={!dataSetId}
-            label={i18n.t('Period')}
-            value={periodId ? selectorBarItemValue : undefined}
-            open={periodOpen}
-            setOpen={setPeriodOpen}
-            noValueMessage={i18n.t('Choose a period')}
-        >
-            {year ? (
-                <>
-                    <YearNavigator
-                        maxYear={maxYear}
-                        year={year}
-                        onYearChange={(year) => {
-                            setPeriodId(null)
-                            setYear(year)
-                        }}
-                    />
+        <div>
+            <DisabledTooltip>
+                <SelectorBarItem
+                    disabled={!dataSetId}
+                    label={i18n.t('Period')}
+                    value={periodId ? selectorBarItemValue : undefined}
+                    open={periodOpen}
+                    setOpen={setPeriodOpen}
+                    noValueMessage={i18n.t('Choose a period')}
+                >
+                    {year ? (
+                        <>
+                            <YearNavigator
+                                maxYear={maxYear}
+                                year={year}
+                                onYearChange={(year) => {
+                                    setPeriodId(null)
+                                    setYear(year)
+                                }}
+                            />
 
-                    <PeriodMenu
-                        onChange={({ selected }) => {
-                            setPeriodId(selected)
-                            setPeriodOpen(false)
-                        }}
-                        periodType={dataSetPeriodType.data}
-                        year={year}
-                    />
-                </>
-            ) : (
-                <div />
-            )}
-        </SelectorBarItem>
+                            <PeriodMenu
+                                onChange={({ selected }) => {
+                                    setPeriodId(selected)
+                                    setPeriodOpen(false)
+                                }}
+                                periodType={dataSetPeriodType.data}
+                                year={year}
+                            />
+                        </>
+                    ) : (
+                        <div />
+                    )}
+                </SelectorBarItem>
+            </DisabledTooltip>
+        </div>
     )
 }
