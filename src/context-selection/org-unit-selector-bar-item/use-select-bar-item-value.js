73f14bb81823f822fed6a1d4861d47f1bf5029f0
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-01-27 14:59:57 +0100

    refactor(org unit selector): remove filtering by data set org units

--- src/context-selection/org-unit-selector-bar-item/use-select-bar-item-value.js
+++ src/context-selection/org-unit-selector-bar-item/use-select-bar-item-value.js
@@ -1,22 +1,27 @@
 import i18n from '@dhis2/d2-i18n'
-import useDataSetOrgUnitPaths from './use-data-set-org-unit-paths.js'
 import useOrgUnit from './use-organisation-unit.js'
 import useUserOrgUnits from './use-user-org-units.js'
 
 export default function useSelectorBarItemValue() {
     const orgUnit = useOrgUnit()
     const userOrgUnits = useUserOrgUnits()
-    const dataSetOrgUnitPaths = useDataSetOrgUnitPaths()
+    // @TODO: Figure out how to only use org units that are connected to the
+    // data set.
+    // const dataSetOrgUnitPaths = useDataSetOrgUnitPaths()
 
     if (
         userOrgUnits.loading ||
-        orgUnit.loading ||
-        dataSetOrgUnitPaths.loading
+        orgUnit.loading
+        // || dataSetOrgUnitPaths.loading
     ) {
         return i18n.t('Fetching organisation unit info')
     }
 
-    if (orgUnit.error || userOrgUnits.error || dataSetOrgUnitPaths.error) {
+    if (
+        orgUnit.error ||
+        userOrgUnits.error
+        // || dataSetOrgUnitPaths.error
+    ) {
         return i18n.t('Error occurred while loading organisation unit info')
     }
 
