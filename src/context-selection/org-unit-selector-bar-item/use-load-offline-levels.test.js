bfb235002e028b8b177b3124415d4b9bc66bdd11
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-02-24 13:30:50 +0100

    test: test loading organisation unit offline levels

---
+++ src/context-selection/org-unit-selector-bar-item/use-load-offline-levels.test.js
@@ -0,0 +1,68 @@
+import { useDataEngine } from '@dhis2/app-runtime'
+import { renderHook } from '@testing-library/react-hooks'
+import loadOfflineLevel from './load-offline-level.js'
+import useLoadOfflineLevels from './use-load-offline-levels.js'
+import useOfflineLevelsToLoad from './use-offline-levels-to-load.js'
+import useOrganisationUnitLevels from './use-organisation-unit-levels.js'
+
+jest.mock('@dhis2/app-runtime', () => ({
+    useDataEngine: jest.fn(),
+}))
+
+jest.mock('./load-offline-level.js', () => ({
+    __esModule: true,
+    default: jest.fn(),
+}))
+
+jest.mock('./use-offline-levels-to-load.js', () => ({
+    __esModule: true,
+    default: jest.fn(),
+}))
+
+jest.mock('./use-organisation-unit-levels.js', () => ({
+    __esModule: true,
+    default: jest.fn(),
+}))
+
+describe('useLoadOfflineLevels', () => {
+    const dataEngine = {}
+    useDataEngine.mockImplementation(() => dataEngine)
+
+    const offlineLevelToLoad = {
+        data: [
+            { offlineLevels: 2, id: 'foo' },
+            { offlineLevels: 1, id: 'bar' },
+        ],
+    }
+    useOfflineLevelsToLoad.mockImplementation(() => offlineLevelToLoad)
+
+    afterEach(() => {
+        useDataEngine.mockClear(), loadOfflineLevel.mockClear()
+        useOfflineLevelsToLoad.mockClear()
+        useOrganisationUnitLevels.mockClear()
+    })
+
+    it('should call "loadOfflineLevel" for each offline level to load', async () => {
+        const { result, waitForNextUpdate } = renderHook(useLoadOfflineLevels)
+
+        await waitForNextUpdate()
+
+        expect(loadOfflineLevel).toHaveBeenCalledTimes(2)
+        expect(loadOfflineLevel).toHaveBeenNthCalledWith(1, {
+            dataEngine,
+            offlineLevels: 2,
+            id: 'foo',
+        })
+        expect(loadOfflineLevel).toHaveBeenNthCalledWith(2, {
+            dataEngine,
+            offlineLevels: 1,
+            id: 'bar',
+        })
+
+        expect(result.current).toEqual({
+            loading: false,
+            error: undefined,
+            data: true,
+        })
+    })
+})
