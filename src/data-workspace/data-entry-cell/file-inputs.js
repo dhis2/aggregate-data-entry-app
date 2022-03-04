27e6f45101e2b47597f1bbc269b6bf4689ae8049
Kai Vandivier <kpvandivier@gmail.com>
2022-03-04 18:54:01 +0100

    feat: use react query for fileResource query

--- src/data-workspace/data-entry-cell/file-inputs.js
+++ src/data-workspace/data-entry-cell/file-inputs.js
@@ -1,23 +1,14 @@
-// todo: use RQ
-import { useDataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import { colors, Button, FileInput, IconAttachment16 } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { useField } from 'react-final-form'
+import { useQuery } from 'react-query'
 import styles from './inputs.module.css'
 import {
     MUTATION_TYPES,
     useDataValueMutation,
 } from './use-data-value-mutation.js'
-
-// This endpoint doesn't support field filtering
-const FILE_META_QUERY = {
-    fileResource: {
-        resource: 'fileResources',
-        id: ({ id }) => id,
-    },
-}
 
 const getFileSize = (file) => {
     return `${(file.size / 1024).toFixed(2)} KB`
@@ -40,32 +31,26 @@ export const FileResourceInput = ({
             onChange: true,
         },
     })
-    // todo: use reactQuery
-    const { data, refetch: getFileMeta } = useDataQuery(FILE_META_QUERY, {
-        lazy: true,
-    })
+    const { data } = useQuery(
+        // This endpoint doesn't support field filtering :(
+        ['fileResources', { id: input.value }],
+        {
+            enabled:
+                !!input.value &&
+                meta.pristine &&
+                typeof input.value === 'string',
+        }
+    )
     const { mutate: uploadFile } = useDataValueMutation(
         MUTATION_TYPES.FILE_UPLOAD
     )
     const { mutate: deleteFile } = useDataValueMutation(MUTATION_TYPES.DELETE)
-
-    React.useEffect(() => {
-        // If a file is already stored for this data value, fetch some metadata
-        // (input.value will be populated with a fileResource UID)
-        if (input.value && meta.pristine && typeof input.value === 'string') {
-            getFileMeta({ id: input.value })
-        }
-        // Note that while offline, the dataValueSets response will be updated
-        // such that the value of this data item is { name, size } (instead of
-        // just an ID), so the meta info can be shown for the pending upload
-    }, [input.value])
 
     const handleChange = ({ files }) => {
         const newFile = files[0]
         input.onChange({ name: newFile.name, size: newFile.size })
         input.onBlur()
         if (newFile instanceof File) {
-            // todo: optimistically update RQ cache
             setIsFileLoading(true)
             setIsFileSynced(false)
             uploadFile(
@@ -101,8 +86,8 @@ export const FileResourceInput = ({
         ? input.value
         : data && input.value // i.e. if value is a resource UID
         ? {
-              name: data.fileResource.name,
-              size: data.fileResource.contentLength,
+              name: data.name,
+              size: data.contentLength,
           }
         : null
 
