2bbfd4e6c96cc4423c1ac3dcbfc37e00dc3819cb
Birk Johansson <birk@dhis2.org>
2022-05-03 16:51:31 +0200

    refactor: cleanup

--- src/app/load-app.js
+++ src/app/load-app.js
@@ -4,11 +4,7 @@ import React from 'react'
 import { useCustomFormsPrefetch } from '../custom-forms/index.js'
 import { useMetadata } from '../metadata/index.js'
 import css from './load-app.module.css'
-/**
- * "use-query-params" requires a router. It suggests react-router-dom in the
- * docs, so that's why I chose that one. Unfortunately it doesn't work with
- * react-router-dom@6, so I have do go with react-router-dom@^5
- */
+
 const LoadApp = ({ children }) => {
     useCustomFormsPrefetch()
     const { isLoading, isError, data } = useMetadata()
