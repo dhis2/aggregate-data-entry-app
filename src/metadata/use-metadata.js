364316240819318076d3383f76baa8544077324d
Birk Johansson <birk@dhis2.org>
2022-05-04 13:33:23 +0200

    fix(metadata): staleTime to 24hrs

--- src/metadata/use-metadata.js
+++ src/metadata/use-metadata.js
@@ -12,7 +12,7 @@ const queryKey = [`/dataEntry/metadata`]
 const queryOpts = {
     refetchOnMount: false,
     select: selectorFunction,
-    staleTime: 24 * 60 * 1000,
+    staleTime: 24 * 60 * 60 * 1000,
     meta: {
         persist: true,
     },
