6a2bfc5b04c71f8ed3a3f7f43d5ca88436b163d4
Birk Johansson <birk@dhis2.org>
2022-05-06 13:00:51 +0200

    refactor: cleanup

--- src/context-selection/attribute-option-combo-selector-bar-item/index.js
+++ src/context-selection/attribute-option-combo-selector-bar-item/index.js
@@ -1,2 +1,1 @@
 export { default as AttributeOptionComboSelectorBarItem } from './attribute-option-combo-selector-bar-item.js'
-export { useApiAttributeParams } from '../../data-workspace/use-api-attribute-params.js'
