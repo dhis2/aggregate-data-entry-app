23754671c3cc8f1bbc15963768816890431ecd45
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-05-10 08:07:16 +0200

    fix(section filter): check org existence of org unit id and period id

--- src/context-selection/section-filter-selector-bar-item/use-should-component-render-null.js
+++ src/context-selection/section-filter-selector-bar-item/use-should-component-render-null.js
@@ -1,22 +1,17 @@
 import { selectors, useMetadata } from '../../metadata/index.js'
-import { useDataSetId } from '../use-context-selection/index.js'
+import { useDataSetId, useOrgUnitId, usePeriodId } from '../use-context-selection/index.js'
 
-/*
- * `dataSetSectionsInfo.loading` does not mean that "null" should be rendered.
- * This happens automatically during the first loading as
- * `'SECTION' !== dataSetSectionsInfo.data?.formType` resolves to `true`.
- *
- * But when data is stale and this function has determined that the component
- * should not render `null`, then it's most likely not going to change when
- * reloading
- */
 export default function useShouldComponentRenderNull() {
     const [dataSetId] = useDataSetId()
+    const [orgUnitId] = useOrgUnitId()
+    const [periodId] = usePeriodId()
     const { data: metadata } = useMetadata()
     const dataSet = selectors.getDataSets(metadata)[dataSetId]
 
     return (
         !dataSet ||
+        !orgUnitId ||
+        !periodId ||
         'SECTION' !== dataSet.formType ||
         dataSet.sections.length === 0
     )
