e53b7d97de01634c4af7b1e0749424b28796975c
Birk Johansson <birk@dhis2.org>
2022-05-12 16:05:37 +0200

    fix: add total-cells components

---
+++ src/data-workspace/category-combo-table/total-cells.js
@@ -0,0 +1,46 @@
+import i18n from '@dhis2/d2-i18n'
+import { TableCell, TableCellHead, TableRow } from '@dhis2/ui'
+import propTypes from 'prop-types'
+import React from 'react'
+import styles from './category-combo-table.module.css'
+
+export const TotalCell = ({ children }) => (
+    <TableCell className={styles.totalCell}>{children}</TableCell>
+)
+
+TotalCell.propTypes = {
+    children: propTypes.node,
+}
+
+export const TotalHeader = ({ rowSpan }) => (
+    <TableCellHead className={styles.totalHeader} rowSpan={rowSpan}>
+        {i18n.t('Totals')}
+    </TableCellHead>
+)
+
+TotalHeader.propTypes = {
+    rowSpan: propTypes.number,
+}
+
+export const ColumnTotals = ({ columnTotals, renderTotalSum }) => {
+    return (
+        <TableRow>
+            <TableCellHead className={styles.totalHeader}>
+                {i18n.t('Totals')}
+            </TableCellHead>
+            {columnTotals.map((v, i) => (
+                <TotalCell key={i}>{v}</TotalCell>
+            ))}
+            {renderTotalSum && (
+                <TotalCell>
+                    {columnTotals.reduce((acc, curr) => acc + curr)}
+                </TotalCell>
+            )}
+        </TableRow>
+    )
+}
+
+ColumnTotals.propTypes = {
+    columnTotals: propTypes.arrayOf(propTypes.number),
+    renderTotalSum: propTypes.bool,
+}
