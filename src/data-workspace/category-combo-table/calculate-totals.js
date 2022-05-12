d224e5d2b0cded62c71a35872743e1085ea4eafc
Birk Johansson <birk@dhis2.org>
2022-05-12 17:03:47 +0200

    fix(calculateColumnTotals): map to number when only one dataElement

--- src/data-workspace/category-combo-table/calculate-totals.js
+++ src/data-workspace/category-combo-table/calculate-totals.js
@@ -1,11 +1,13 @@
 export const calculateColumnTotals = (matrix) =>
-    matrix.reduce((totals, currRow) =>
-        totals.map((currTotal, i) => {
-            const a = Number(currRow[i]) || 0
-            const b = Number(currTotal) || 0
-            return a + b
-        })
-    )
+    matrix.length === 1
+        ? matrix[0].map((v) => Number(v) || 0)
+        : matrix.reduce((totals, currRow) =>
+              totals.map((currTotal, i) => {
+                  const a = Number(currRow[i]) || 0
+                  const b = Number(currTotal) || 0
+                  return a + b
+              })
+          )
 
 export const calculateRowTotals = (matrix) =>
     matrix.map((row) =>
