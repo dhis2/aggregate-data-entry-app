44025fcb42340bff0102c11c6211b3f01a5520d5
Birk Johansson <birk@dhis2.org>
2022-06-01 15:02:58 +0200

    refactor: calculateColumnTotals

--- src/data-workspace/category-combo-table/calculate-totals.js
+++ src/data-workspace/category-combo-table/calculate-totals.js
@@ -1,14 +1,12 @@
-export const calculateColumnTotals = (matrix) =>
-    matrix.length < 2
-        ? matrix[0].map((v) => Number(v) || 0)
-        : matrix.reduce((totals, currRow) =>
-              totals.map((currTotal, i) => sum(currTotal, currRow[i]))
-          )
-
-const sum = (acc, curr) => {
-    const a = Number(curr) || 0
-    const b = Number(acc) || 0
-    return a + b
+const sum = (acc, value) => {
+    const b = Number(value) || 0
+    return acc + b
 }
 
-export const calculateRowTotal = (matrix, row = 0) => matrix[row].reduce(sum)
+const calculateColumnTotal = (matrix, column = 0) =>
+    matrix.reduce((total, currRow) => sum(total, currRow[column]), 0)
+
+export const calculateColumnTotals = (matrix) =>
+    matrix[0]?.map((_, i) => calculateColumnTotal(matrix, i)) || []
+
+export const calculateRowTotal = (matrix, row = 0) => matrix[row].reduce(sum, 0)
