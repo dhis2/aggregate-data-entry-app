a447a1f625f2f6f943dc409fe7de668420552b36
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-06-23 10:00:09 +0200

    refactor(use data value mutation): copy shared code into hook

--- src/data-workspace/use-data-value-mutation/optimistically-delete-data-value.js
+++ src/data-workspace/use-data-value-mutation/optimistically-delete-data-value.js
@@ -6,19 +6,11 @@ export default async function optimisticallyDeleteDataValue({
     newDataValue,
     dataValueSetQueryKey,
 }) {
-    // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-    await queryClient.cancelQueries(dataValueSetQueryKey)
-
-    // Snapshot the previous value
-    const previousDataValueSet = queryClient.getQueryData(dataValueSetQueryKey)
-
     // Optimistically update to the new value
-    queryClient.setQueryData(dataValueSetQueryKey, () => {
+    queryClient.setQueryData(dataValueSetQueryKey, (previousDataValueSet) => {
         // dataValueSet.dataValues can be undefined
         const previousDataValues = previousDataValueSet.dataValues || []
         const matchIndex = getDataValueIndex(previousDataValues, newDataValue)
         return deleteDataValue(previousDataValueSet, matchIndex)
     })
-
-    return { previousDataValueSet, dataValueSetQueryKey }
 }
