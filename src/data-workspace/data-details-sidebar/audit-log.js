26b629cf34889526ca5eab19c8cbfce2649acebe
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-07-04 13:39:12 +0200

    refactor(audit log): use data table for display

--- src/data-workspace/data-details-sidebar/audit-log.js
+++ src/data-workspace/data-details-sidebar/audit-log.js
@@ -1,59 +1,44 @@
 import i18n from '@dhis2/d2-i18n'
-import { CircularLoader, Tooltip } from '@dhis2/ui'
+import {
+    CircularLoader,
+    DataTable,
+    DataTableCell,
+    DataTableColumnHeader,
+    DataTableRow,
+    TableBody,
+    TableHead,
+    Tag,
+} from '@dhis2/ui'
 import moment from 'moment'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { ExpandableUnit } from '../../shared/index.js'
 import styles from './audit-log.module.css'
 
-const renderMessage = ({ modifiedBy, auditType, value, prevValue }) => {
-    if (auditType === 'UPDATE' && typeof prevValue === 'undefined') {
-        return i18n.t('{{user}} set to {{value}}', { user: modifiedBy, value })
-    }
-
-    if (auditType === 'UPDATE') {
-        return i18n.t('{{user}} updated to {{value}} (was {{prevValue}})', {
-            user: modifiedBy,
-            value,
-            prevValue,
-        })
-    }
-
-    if (auditType === 'DELETE') {
-        return i18n.t('{{user}} deleted (was {{prevValue}})', {
-            user: modifiedBy,
-            value,
-            prevValue,
-        })
-    }
-
-    return auditType
-}
-
-const Entry = ({ entry }) => {
-    const timeAgo = moment(entry.created).fromNow()
-
-    return (
-        <>
-            <span className={styles.entryMessage}>{renderMessage(entry)}</span>
-            <Tooltip content={entry.created}>
-                <span className={styles.entryTimeAgo}>{timeAgo}</span>
-            </Tooltip>
-        </>
-    )
-}
-
-Entry.propTypes = {
-    entry: PropTypes.shape({
-        auditType: PropTypes.oneOf(['UPDATE', 'DELETE']).isRequired,
-        created: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired,
-        prevValue: PropTypes.string,
-    }).isRequired,
-}
+const title = i18n.t('Audit log')
 
 const AuditLogUnit = ({ loading, audits }) => {
+
+    if (loading) {
+        return (
+            <ExpandableUnit title={title}>
+                {loading && <CircularLoader small />}
+            </ExpandableUnit>
+        )
+    }
+
     const isEmptyAuditLog = !Array.isArray(audits) || audits.length === 0
+
+    if (isEmptyAuditLog) {
+        return (
+            <ExpandableUnit title={title}>
+                <p className={styles.placeholder}>
+                    {i18n.t('No audit log for this data item.')}
+                </p>
+            </ExpandableUnit>
+        )
+    }
+
     // Do not modify original array, sort mutates the array
     const newToOldAuditLog = [...audits].sort((left, right) => {
         const lCreated = new Date(left.created)
@@ -61,36 +46,59 @@ const AuditLogUnit = ({ loading, audits }) => {
         return lCreated === rCreated ? 0 : lCreated < rCreated ? 1 : -1
     })
 
-    return (
-        <ExpandableUnit title={i18n.t('Audit log')}>
-            {loading && <CircularLoader small />}
-
-            {!loading && isEmptyAuditLog && (
+    if (!newToOldAuditLog.length) {
+        return (
+            <ExpandableUnit title={title}>
                 <p className={styles.placeholder}>
-                    {i18n.t('No audit log for this data item.')}
+                    {i18n.t('No audit log for this data item')}
                 </p>
-            )}
+            </ExpandableUnit>
+        )
+    }
 
-            {!loading && !isEmptyAuditLog && (
-                <ul className={styles.entries}>
-                    {!newToOldAuditLog.length && (
-                        <span>{i18n.t('No audit log for this data item')}</span>
-                    )}
+    return (
+        <ExpandableUnit title={title}>
+            <DataTable>
+                <TableHead>
+                    <DataTableRow>
+                        <DataTableColumnHeader>
+                            Date
+                        </DataTableColumnHeader>
+                        <DataTableColumnHeader>
+                            User
+                        </DataTableColumnHeader>
+                        <DataTableColumnHeader>
+                            New value
+                        </DataTableColumnHeader>
+                    </DataTableRow>
+                </TableHead>
 
-                    {newToOldAuditLog.map((entry, index, all) => {
-                        const payload = {
-                            ...entry,
-                            prevValue: all[index + 1]?.value,
-                        }
+                <TableBody>
+                    {newToOldAuditLog.map((entry, index) => {
+                        const { modifiedBy: user, value, created, auditType } = entry
 
                         return (
-                            <li key={index} className={styles.entry}>
-                                <Entry entry={payload} />
-                            </li>
+                            <DataTableRow key={index}>
+                                <DataTableCell>
+                                    {moment(created).format('YYYY-MM-DD HH:mm')}
+                                </DataTableCell>
+                                <DataTableCell>
+                                    {user}
+                                </DataTableCell>
+                                <DataTableCell>
+                                    <div style={{ textAlign: 'right' }}>
+                                        {
+                                            auditType === 'DELETE'
+                                                ? <Tag negative>{i18n.t('Deleted')}</Tag>
+                                                : value
+                                        }
+                                    </div>
+                                </DataTableCell>
+                            </DataTableRow>
                         )
                     })}
-                </ul>
-            )}
+                </TableBody>
+            </DataTable>
         </ExpandableUnit>
     )
 }
@@ -102,7 +110,7 @@ AuditLogUnit.defaultProps = {
 AuditLogUnit.propTypes = {
     audits: PropTypes.arrayOf(
         PropTypes.shape({
-            auditType: PropTypes.string,
+            auditType: PropTypes.oneOf(['UPDATE', 'DELETE']).isRequired,
             created: PropTypes.string,
             modifiedBy: PropTypes.string,
             prevValue: PropTypes.string,
