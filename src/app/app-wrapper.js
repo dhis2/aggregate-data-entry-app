925c1d5b80b007912b590325030aac4979ffcb08
Birk Johansson <birk@dhis2.org>
2022-07-04 14:37:40 +0200

    refactor: separate configured-query-client-provider

--- src/app/app-wrapper.js
+++ src/app/app-wrapper.js
@@ -1,42 +1,19 @@
 import { CssReset, CssVariables } from '@dhis2/ui'
 import React from 'react'
 import { ReactQueryDevtools } from 'react-query/dist/react-query-devtools.development'
-import { PersistQueryClientProvider } from 'react-query/persistQueryClient'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import { QueryParamProvider } from 'use-query-params'
 import { RightHandPanelProvider } from '../right-hand-panel/index.js'
 import { CurrentItemProvider } from '../shared/index.js'
 import App from './app.js'
-import createIDBPersister from './query-client/persister.js'
-import useQueryClient from './query-client/use-query-client.js'
-
-const persister = createIDBPersister()
-
-const persistOptions = {
-    persister,
-    maxAge: Infinity,
-    dehydrateOptions: {
-        dehydrateMutations: true,
-        dehydrateQueries: true,
-        shouldDehydrateQuery: (query) => {
-            const isSuccess = query.state.status === 'success'
-            const shouldPersist = query?.meta?.persist === true
-            return isSuccess && shouldPersist
-        },
-    },
-}
+import { ConfiguredQueryClientProvider } from './query-client/configured-query-client-provider.js'
 
 const AppWrapper = () => {
-    const queryClient = useQueryClient()
-
     return (
         <>
             <CssReset />
             <CssVariables colors spacers theme />
-            <PersistQueryClientProvider
-                client={queryClient}
-                persistOptions={persistOptions}
-            >
+            <ConfiguredQueryClientProvider>
                 <ReactQueryDevtools />
                 <Router>
                     <QueryParamProvider ReactRouterRoute={Route}>
@@ -47,7 +24,7 @@ const AppWrapper = () => {
                         </CurrentItemProvider>
                     </QueryParamProvider>
                 </Router>
-            </PersistQueryClientProvider>
+            </ConfiguredQueryClientProvider>
         </>
     )
 }
