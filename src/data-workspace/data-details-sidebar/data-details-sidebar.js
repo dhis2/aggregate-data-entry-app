a9c30e7a3c9819d8d162e05f2db9514958b26e7b
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-07-07 10:28:28 +0200

    fix(audit and history): load data when open & close when changing item

--- src/data-workspace/data-details-sidebar/data-details-sidebar.js
+++ src/data-workspace/data-details-sidebar/data-details-sidebar.js
@@ -1,26 +1,18 @@
 import i18n from '@dhis2/d2-i18n'
-import { CircularLoader, NoticeBox } from '@dhis2/ui'
 import React from 'react'
-import { useQuery } from 'react-query'
-import {
-    useOrgUnitId,
-    usePeriodId,
-    useIsValidSelection,
-} from '../../context-selection/index.js'
+import { useOrgUnitId } from '../../context-selection/index.js'
 import {
     Sidebar,
     Title,
     ExpandableUnit,
-    useApiAttributeParams,
     useDataValueSet,
     useHighlightedField,
     SidebarProps,
 } from '../../shared/index.js'
-import * as queryKeyFactory from '../query-key-factory.js'
 import AuditLog from './audit-log.js'
 import BasicInformation from './basic-information.js'
 import Comment from './comment.js'
-import HistoryUnit from './history-unit.js'
+import History from './history.js'
 import Limits from './limits.js'
 
 export default function DataDetailsSidebar({ hide }) {
@@ -30,26 +22,11 @@ export default function DataDetailsSidebar({ hide }) {
         ...item,
         ...dataValueSet.data?.dataValues[item.dataElement]?.[item.categoryOptionCombo]
     }
+
     const onMarkForFollowup = () => null
     const onUnmarkForFollowup = () => null
-    const [periodId] = usePeriodId()
+
     const [orgUnitId] = useOrgUnitId()
-    const { attributeCombo, attributeOptions } = useApiAttributeParams()
-
-    const isValidSelection = useIsValidSelection()
-
-    const dataValueContextQueryKey = queryKeyFactory.dataValueContext.byParams({
-        dataElementId: dataValue.dataElement,
-        periodId: periodId,
-        orgUnitId: orgUnitId,
-        categoryOptionIds: attributeOptions,
-        categoryComboId: attributeCombo,
-        categoryOptionComboId: dataValue.categoryOptionCombo,
-    })
-
-    const dataValueContext = useQuery(dataValueContextQueryKey, {
-        enabled: isValidSelection,
-    })
 
     const minMaxValue = dataValueSet.data?.minMaxValues.find((curMinMaxValue) => (
         curMinMaxValue.categoryOptionCombo === dataValue.categoryOptionCombo &&
@@ -65,7 +42,6 @@ export default function DataDetailsSidebar({ hide }) {
     return (
         <Sidebar>
             <Title onClose={hide}>{i18n.t('Details')}</Title>
-
             <BasicInformation
                 item={dataValue}
                 onMarkForFollowup={onMarkForFollowup}
@@ -88,55 +64,8 @@ export default function DataDetailsSidebar({ hide }) {
                 />
             </ExpandableUnit>
 
-            {dataValueContext.isLoading && (
-                <ExpandableUnit
-                    title={i18n.t('History and audit')}
-                    initiallyOpen
-                >
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContext: 'center',
-                        }}
-                    >
-                        <CircularLoader small />
-                    </div>
-                </ExpandableUnit>
-            )}
-
-            {dataValueContext.error && (
-                <ExpandableUnit
-                    title={i18n.t('History and audit')}
-                    initiallyOpen
-                >
-                    <NoticeBox
-                        title={i18n.t(
-                            'Something went wrong loading the history and audit log'
-                        )}
-                    >
-                        <p>{dataValueContext.error.message}</p>
-                    </NoticeBox>
-                </ExpandableUnit>
-            )}
-
-            {dataValueContext.data && (
-                <>
-                    <HistoryUnit
-                        loading={
-                            dataValueContext.isIdle || dataValueContext.loading
-                        }
-                        history={dataValueContext.data?.history}
-                    />
-
-                    <AuditLog
-                        loading={
-                            dataValueContext.isIdle || dataValueContext.loading
-                        }
-                        audits={dataValueContext.data?.audits}
-                    />
-                </>
-            )}
+            <History />
+            <AuditLog />
         </Sidebar>
     )
 }
