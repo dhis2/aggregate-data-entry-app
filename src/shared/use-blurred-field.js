06876b767531df48dcefbe14fbb89eef7859f5e5
Hendrik de Graaf <hendrik@dhis2.org>
2022-07-12 14:25:45 +0200

    fix(use-blurred-field): initialise as undefined, consistent with RFF

--- src/shared/use-blurred-field.js
+++ src/shared/use-blurred-field.js
@@ -2,7 +2,7 @@ import { useMemo, useRef } from 'react'
 import { useFormState } from 'react-final-form'
 
 export const useBlurredField = () => {
-    const previouslyActiveFieldRef = useRef(null)
+    const previouslyActiveFieldRef = useRef(undefined)
     const { active } = useFormState({ subscription: { active: true } })
 
     return useMemo(() => {
