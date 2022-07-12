10830cd0f5992e7cab9a15496771784e49ea07f6
Hendrik de Graaf <hendrik@dhis2.org>
2022-07-12 14:24:07 +0200

    fix(use-blurred-field): prevent using state and allow undefined value
    When focussing on a field in the form, then focussing something outside
    of the form, and then focussing on a field in the form again, the
    correct sequence of events for the values of the blurred field should
    be:
    1. undefined: nothing focussed
    2. undefined: first form field active, but no form field blurred
    3. active-field-id: first field blurred
    4. undefined: next form field active, but no form-field was blurred

--- src/shared/use-blurred-field.js
+++ src/shared/use-blurred-field.js
@@ -1,18 +1,13 @@
-import { useState, useEffect, useRef } from 'react'
+import { useMemo, useRef } from 'react'
 import { useFormState } from 'react-final-form'
 
 export const useBlurredField = () => {
     const previouslyActiveFieldRef = useRef(null)
-    const [blurredField, setBlurredField] = useState(null)
     const { active } = useFormState({ subscription: { active: true } })
 
-    useEffect(() => {
-        if (active) {
-            const newBlurredField = previouslyActiveFieldRef.current
-            previouslyActiveFieldRef.current = active
-            setBlurredField(newBlurredField)
-        }
+    return useMemo(() => {
+        const blurredField = previouslyActiveFieldRef.current
+        previouslyActiveFieldRef.current = active
+        return blurredField
     }, [active])
-
-    return blurredField
 }
