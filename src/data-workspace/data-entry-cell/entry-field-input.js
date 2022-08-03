dhis2/data-entry-app

refactor(opening data details sidebar): change shift+enter -> ctrl/cmd+enter

src/data-workspace/data-entry-cell/entry-field-input.js
@@ -128,9 +128,10 @@ export function EntryFieldInput({
     })
 
     const onKeyDown = (event) => {
-        const { key, shiftKey } = event
+        const { key, ctrlKey, metaKey } = event
+        const ctrlOrMetaKey = ctrlKey ^ metaKey
 
-        if (shiftKey && key === 'Enter') {
+        if (ctrlOrMetaKey && key === 'Enter') {
             rightHandPanel.show('data-details')
         } else if (key === 'ArrowDown' || key === 'Enter') {
             event.preventDefault()

dhis2/aggregate-data-entry-app

src/data-workspace/data-entry-cell/entry-field-input.js
@@ -74,9 +74,10 @@ export function EntryFieldInput({
 
     const onKeyDown = useCallback(
         (event) => {
-            const { key, shiftKey } = event
+            const { key, ctrlKey, metaKey } = event
+            const ctrlXorMetaKey = ctrlKey ^ metaKey
 
-            if (shiftKey && key === 'Enter') {
+            if (ctrlXorMetaKey && key === 'Enter') {
                 setRightHandPanel(dataDetailsSidebarId)
             } else if (key === 'ArrowDown' || key === 'Enter') {
                 event.preventDefault()