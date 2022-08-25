a29c303083067162deb75917527f33d1c705fc7c
Kai Vandivier <kpvandivier@gmail.com>
2022-08-25 16:36:40 +0200

    fix: use correct RQ import

--- src/data-workspace/data-value-mutations/data-value-mutations.js
+++ src/data-workspace/data-value-mutations/data-value-mutations.js
@@ -1,4 +1,4 @@
-import { useQueryClient, useMutation } from 'react-query'
+import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { useDataValueSetQueryKey } from '../../shared/index.js'
 import {
     optimisticallyDeleteDataValue,
