242755d471f860c0ea5e674380e68e69e35598dc
Hendrik de Graaf <hendrik@dhis2.org>
2022-09-06 10:47:16 +0200

    fix: prevent value undefined and intialise lastSyncedValue from input

--- src/data-workspace/inputs/generic-input.js
+++ src/data-workspace/inputs/generic-input.js
@@ -1,6 +1,6 @@
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useField } from 'react-final-form'
 import { NUMBER_TYPES, VALUE_TYPES } from '../../shared/index.js'
 import { useSetDataValueMutation } from '../data-value-mutations/index.js'
@@ -27,7 +27,27 @@ export const GenericInput = ({
     onFocus,
     disabled,
 }) => {
-    const [lastSyncedValue, setLastSyncedValue] = useState('')
+    const formatValue = (value) => {
+        if (value === undefined) {
+            return ''
+        } else if (
+            value.trim() &&
+            NUMBER_TYPES.includes(valueType) &&
+            Number.isFinite(Number(value))
+        ) {
+            return Number(value).toString()
+        } else {
+            return value.trim()
+        }
+    }
+    const { input, meta } = useField(fieldname, {
+        validate: validatorsByValueType[valueType],
+        subscription: { value: true, dirty: true, valid: true },
+        format: formatValue,
+        formatOnBlur: true,
+        parse: (value) => value ?? '',
+    })
+    const [lastSyncedValue, setLastSyncedValue] = useState(input.value)
     const { mutate } = useSetDataValueMutation({ deId, cocId })
     const syncData = (value) => {
         // todo: Here's where an error state could be set: ('onError')
@@ -42,35 +62,6 @@ export const GenericInput = ({
             }
         )
     }
-
-    const formatValue = (value) => {
-        if (value === undefined) {
-            return ''
-        } else if (
-            value.trim() &&
-            NUMBER_TYPES.includes(valueType) &&
-            Number.isFinite(Number(value))
-        ) {
-            return Number(value).toString()
-        } else {
-            return value.trim()
-        }
-    }
-
-    const { input, meta } = useField(fieldname, {
-        validate: validatorsByValueType[valueType],
-        subscription: { value: true, dirty: true, valid: true },
-        format: formatValue,
-        formatOnBlur: true,
-    })
-
-    // need to initialize lastSyncedValue
-    useEffect(() => {
-        if (input.value !== undefined) {
-            setLastSyncedValue(input.value)
-        }
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
     const handleBlur = () => {
         const { value } = input
         const formattedValue = formatValue(value)
@@ -83,7 +74,7 @@ export const GenericInput = ({
     return (
         <input
             {...input}
-            value={input.value ?? ''}
+            value={input.value}
             className={cx(styles.basicInput, {
                 [styles.alignToEnd]: NUMBER_TYPES.includes(valueType),
             })}
