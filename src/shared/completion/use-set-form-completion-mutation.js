2f18af21f15627f6c849316ba5d3381b13eea133
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-09-09 11:07:13 +0200

    fix(form completion error alert message): correct & translate message

--- src/shared/completion/use-set-form-completion-mutation.js
+++ src/shared/completion/use-set-form-completion-mutation.js
@@ -1,4 +1,5 @@
 import { useAlert, useDataEngine } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
 import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { useApiAttributeParams } from '../use-api-attribute-params.js'
 import {
@@ -61,6 +62,8 @@ export default function useSetFormCompletionMutation() {
         retry: 0, // @TODO: Is this correct?
         mutationKey: dataValueSetQueryKey,
         onMutate: async ({ variables }) => {
+            const complete = variables.completed
+
             // Cancel any outgoing refetches (so they don't overwrite our optimistic delete)
             await queryClient.cancelQueries(dataValueSetQueryKey)
 
@@ -73,17 +76,28 @@ export default function useSetFormCompletionMutation() {
                 ...previousDataValueSet,
                 completeStatus: {
                     ...previousDataValueSet.completeStatus,
-                    complete: variables.completed,
+                    complete,
                 },
             }))
 
-            const context = { previousDataValueSet, dataValueSetQueryKey }
+            const context = {
+                complete,
+                previousDataValueSet,
+                dataValueSetQueryKey,
+            }
             return context
         },
         onError: (event, _, context) => {
-            showErrorAlert(
-                `Something went wrong while deleting the min-max limits: ${event.message}`
+            const alertMessage = i18n.t(
+                `Something went wrong while setting the form's completion to "{{completed}}": {{errorMessage}}`,
+                {
+                    completed: context.complete,
+                    errorMessage: event.message,
+                    nsSeparater: '-:-',
+                }
             )
+
+            showErrorAlert(alertMessage)
 
             queryClient.setQueryData(
                 context.dataValueSetQueryKey,
