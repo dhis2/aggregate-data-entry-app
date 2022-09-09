882f250029142b1502da2e993d66d76029b4d82e
Birk Johansson <birkbjo@gmail.com>
2022-09-09 14:50:25 +0200

    fix: resume queries when reloading app (#139)
    * fix: resume queries when reloading app
    
    * refactor: dataValue mutationKeys
    
    * refactor: cleanup
    
    * docs: add some comments
    
    * docs: minor
    
    * refactor: move set mutation defaults to mutation-functions
    
    * fix: use correct mutateFn
    
    * refactor: mutationkey-factory

---
+++ src/data-workspace/data-value-mutations/mutation-key-factory.js
@@ -0,0 +1,39 @@
+const createMutationKey =
+    ([rootKey, subScope]) =>
+    (params) => {
+        const mergedState = {
+            ...subScope,
+        }
+        if (params) {
+            mergedState.params = params
+        }
+        return [...rootKey, { ...mergedState }]
+    }
+
+/**
+ * MutationKeys for dataValues.
+ * These are all functions that can be called with the dataValueParams
+ * to narrow down the mutation
+ *
+ * Example keys:
+ *   mutationKeys.all() ==> ['dataValues']
+ *   mutationKeys.update(dvParams)
+ *      ==> ['dataValues', { method: 'update', params: dvParams }]
+ *   mutationKeys.file(dvParams)
+ *      ==> ['dataValues', { method: 'update', entity: 'file', params: dvParams }]
+ *   mutationKeys.delete() ==> ['dataValues', { method: 'delete' }]
+ * etc.
+ *
+ * ['dataValues, { method: update }] will match all keys with update. Eg, even
+    ['dataValues, { method: update, entity: 'file' }]
+    When checking for active mutation (useIsMutating) you can match based on ‘update’
+    and mutations for file will be included in the match
+*/
+
+const rootKey = ['dataValues']
+export const mutationKeys = {
+    all: createMutationKey([rootKey]),
+    update: createMutationKey([rootKey, { method: 'update' }]),
+    file: createMutationKey([rootKey, { method: 'update', entity: 'file' }]),
+    delete: createMutationKey([rootKey, { method: 'delete' }]),
+}
