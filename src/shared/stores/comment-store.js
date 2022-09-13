1e5f30feeca8cde48bd77ec7c60b876cd817e0dd
Birk Johansson <birk@dhis2.org>
2022-09-14 01:55:11 +0200

    fix: misc cleanup

--- src/shared/stores/comment-store.js
+++
@@ -1,8 +0,0 @@
-import create from 'zustand'
-
-export const useValueStore = create((set, get) => ({
-    dataValues: {},
-    getDataValue: ({ dataElementId, cocId }) =>
-        get().dataValues[dataElementId]?.[cocId],
-    setDataValues: (values) => set({ dataValues: values ?? {} }),
-}))
