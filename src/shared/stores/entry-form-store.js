bbe105575ba4dfa78b18bccf2bc565c02bdba4ac
Birk Johansson <birk@dhis2.org>
2022-09-14 02:53:27 +0200

    docs: comment countLeaves

--- src/shared/stores/entry-form-store.js
+++ src/shared/stores/entry-form-store.js
@@ -11,6 +11,12 @@ export const useEntryFormStore = create((set, get) => ({
     getNumberOfErrors: () => countLeaves(get().getErrors()),
 }))
 
+// errors object is the same shape as form-Values
+// eg. { [dataElementId] : {
+//         [categoryOptionComboId]: "error message"
+//       }
+//     }
+// so we just count the number of leaves in the object to get number of errors
 function countLeaves(object) {
     // base case
     if (Array.isArray(object) || typeof object !== 'object') {
