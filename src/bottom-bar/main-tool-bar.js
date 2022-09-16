5d5a9974d8db32312a1868f39dac7eefe6e01fa9
Thomas Zemp <tom@dhis2.org>
2022-09-16 14:20:24 +0200

    fix(validation button): update run validation button text [TECH-1387]

--- src/bottom-bar/main-tool-bar.js
+++ src/bottom-bar/main-tool-bar.js
@@ -79,7 +79,9 @@ export default function MainToolBar() {
                     className={styles.toolbarItem}
                     onClick={validate}
                 >
-                    {i18n.t('Run validation')}
+                    {rightHandPanel.id === validationResultsSidebarId
+                        ? i18n.t('Hide validation')
+                        : i18n.t('Run validation')}
                 </Button>
             </ValidationButtonTooltip>
 
