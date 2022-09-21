972064917d217ee8d6f9369e57989b6e8128bee8
Hendrik de Graaf <hendrik@dhis2.org>
2022-09-21 21:30:17 +0200

    fix: adjust tests to implementation fix minor implementation bugs

--- src/context-selection/org-unit-selector-bar-item/use-prefetched-organisation-units.js
+++ src/context-selection/org-unit-selector-bar-item/use-prefetched-organisation-units.js
@@ -1,101 +1,5 @@
 import { useQueries } from '@tanstack/react-query'
-
-const computeDeepestLevel = (rootLevel, offlineLevels, configOfflineLevel) => {
-    if (offlineLevels) {
-        return rootLevel + offlineLevels - 1
-    }
-    if (configOfflineLevel) {
-        return configOfflineLevel
-    }
-    return 1
-}
-
-const computeOfflineLevels = (
-    userOrgUnitRoot,
-    filledOrganisationUnitLevels,
-    configOfflineOrgUnitLevel
-) => {
-    const filledOrganisationUnitLevel = filledOrganisationUnitLevels.find(
-        (level) => level === userOrgUnitRoot.level
-    )
-    const deepestLevel = computeDeepestLevel(
-        userOrgUnitRoot,
-        filledOrganisationUnitLevel?.offlineLevels,
-        configOfflineOrgUnitLevel
-    )
-
-    return Array.from(
-        { length: deepestLevel - userOrgUnitRoot.level + 1 },
-        (_, i) => userOrgUnitRoot.level + i
-    )
-}
-
-const useOfflineLevels = () => {
-    const results = useQueries({
-        queries: [
-            {
-                queryKey: [
-                    'me',
-                    {
-                        params: {
-                            fields: ['organisationUnits[id,level,path]'],
-                        },
-                    },
-                ],
-                select: (data) =>
-                    data.organisationUnits.map(({ id, level }) => ({
-                        id,
-                        level,
-                    })),
-            },
-            {
-                queryKey: [
-                    'filledOrganisationUnitLevels',
-                    {
-                        params: {
-                            // @TODO: api ignores the `fields` query param
-                            // See: https://jira.dhis2.org/browse/TECH-973
-                            fields: ['level', 'offlineLevels'],
-                        },
-                    },
-                ],
-                select: (data) =>
-                    data.map(({ id, level, offlineLevels }) => ({
-                        id,
-                        level,
-                        offlineLevels,
-                    })),
-            },
-            {
-                queryKey: ['configuration/offlineOrganisationUnitLevel'],
-                select: ({ level }) => level,
-            },
-        ],
-    })
-    const loading = results.some(({ isLoading }) => isLoading)
-    const error = results.find(({ error }) => error)
-    const [
-        userOrgUnitRoots,
-        filledOrganisationUnitLevels,
-        configOfflineOrgUnitLevel,
-    ] = results.map(({ data }) => data)
-
-    return {
-        loading,
-        error,
-        offlineLevels:
-            !loading && !error
-                ? userOrgUnitRoots.reduce((acc, userOrgUnitRoot) => {
-                      acc[userOrgUnitRoot.id] = computeOfflineLevels(
-                          userOrgUnitRoot,
-                          filledOrganisationUnitLevels,
-                          configOfflineOrgUnitLevel
-                      )
-                      return acc
-                  }, {})
-                : {},
-    }
-}
+import useOfflineLevels from './use-offline-levels.js'
 
 const createPrefetchQueryArgs = ([id, offlineLevels]) => ({
     queryKey: [
@@ -123,11 +27,13 @@ const createPrefetchQueryArgs = ([id, offlineLevels]) => ({
 export default function usePrefetchedOrganisationUnits() {
     const { loading, error, offlineLevels } = useOfflineLevels()
     const results = useQueries({
-        queries: Object.entries(offlineLevels).map(createPrefetchQueryArgs),
+        queries: Object.entries(offlineLevels ?? {}).map(
+            createPrefetchQueryArgs
+        ),
     })
 
     const anyLoading = loading || results.some(({ isLoading }) => isLoading)
-    const anyError = error || results.find(({ error }) => error)
+    const anyError = error || results?.find(({ error }) => error)
     return {
         loading: anyLoading,
         error: anyError,
--- src/context-selection/org-unit-selector-bar-item/use-prefetched-organisation-units.test.js
+++ src/context-selection/org-unit-selector-bar-item/use-prefetched-organisation-units.test.js
@@ -1,79 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQueries } from '@tanstack/react-query'
 import { renderHook } from '@testing-library/react-hooks'
-import useLoadConfigOfflineOrgUnitLevel from './use-load-config-offline-org-unit-level.js'
-import useOrganisationUnitLevels from './use-organisation-unit-levels.js'
+import useOfflineLevels from './use-offline-levels.js'
 import usePrefetchedOrganisationUnits from './use-prefetched-organisation-units.js'
-import useUserOrgUnits from './use-user-org-units.js'
 
 jest.mock('@tanstack/react-query', () => ({
-    useQuery: jest.fn(),
+    useQueries: jest.fn(),
 }))
 
-jest.mock('./use-load-config-offline-org-unit-level.js', () => ({
+jest.mock('./use-offline-levels.js', () => ({
     __esModule: true,
     default: jest.fn(),
 }))
-
-jest.mock('./use-organisation-unit-levels.js', () => ({
-    __esModule: true,
-    default: jest.fn(),
-}))
-
-jest.mock('./use-user-org-units.js', () => ({
-    __esModule: true,
-    default: jest.fn(),
-}))
-
-const fixtures = {
-    userOrgUnits: {
-        organisationUnits: [
-            {
-                path: '/ImspTQPwCqd',
-                id: 'ImspTQPwCqd',
-                level: 1,
-            },
-        ],
-    },
-    organisationUnitLevels: [
-        {
-            level: 1,
-            displayName: 'National',
-        },
-        {
-            level: 2,
-            displayName: 'District',
-        },
-        {
-            level: 3,
-            displayName: 'Chiefdom',
-        },
-        {
-            level: 4,
-            displayName: 'Facility',
-        },
-    ],
-    configOfflineOrgUnitLevel: {
-        level: 3,
-        displayName: 'Chiefdom',
-    },
-}
 
 describe('usePrefetchedOrganisationUnits', () => {
-    it('should not fetch offline units while userOrgUnits are loading', () => {
-        const useQueryMock = jest.fn(() => ({ data: undefined }))
-        useQuery.mockImplementationOnce(useQueryMock)
-        useUserOrgUnits.mockReturnValueOnce({
+    it('should not fetch offline units while offline levels are loading', () => {
+        const useOfflineLevelsMock = jest.fn(() => ({
+            offlineOrganisationUnits: undefined,
+            offlineLevels: undefined,
             loading: true,
-        })
-        useOrganisationUnitLevels.mockReturnValueOnce({
-            loading: false,
-        })
-        useLoadConfigOfflineOrgUnitLevel.mockReturnValueOnce({
-            loading: false,
-        })
+            error: undefined,
+        }))
+        useOfflineLevels.mockImplementationOnce(useOfflineLevelsMock)
+        const useQueriesMock = jest.fn(() => [{ loading: true, data: {} }])
+        useQueries.mockImplementationOnce(useQueriesMock)
 
         const { result } = renderHook(usePrefetchedOrganisationUnits)
-        const args = useQueryMock.mock.calls[0]
 
         expect(result.current).toEqual({
             organisationUnits: undefined,
@@ -81,112 +32,84 @@ describe('usePrefetchedOrganisationUnits', () => {
             loading: true,
             error: undefined,
         })
-        expect(useQueryMock).toHaveBeenCalledTimes(1)
-        expect(args.length).toBe(2)
-        expect(args[1]).toEqual(
-            expect.objectContaining({
-                enabled: false,
-            })
-        )
+        expect(useOfflineLevelsMock).toHaveBeenCalledTimes(1)
+        expect(useQueriesMock).toHaveBeenCalledTimes(1)
+        expect(useQueriesMock).toHaveBeenCalledWith({ queries: [] })
     })
-    it('should not fetch offline units while organisationUnitLevels are loading', () => {
-        const useQueryMock = jest.fn(() => ({ data: undefined }))
-        useQuery.mockImplementationOnce(useQueryMock)
-        useUserOrgUnits.mockReturnValueOnce({
-            loading: false,
-        })
-        useOrganisationUnitLevels.mockReturnValueOnce({
-            loading: true,
-        })
-        useLoadConfigOfflineOrgUnitLevel.mockReturnValueOnce({
-            loading: false,
-        })
-
-        const { result } = renderHook(usePrefetchedOrganisationUnits)
-        const args = useQueryMock.mock.calls[0]
-
-        expect(result.current).toEqual({
-            organisationUnits: undefined,
-            offlineLevels: undefined,
-            loading: true,
-            error: undefined,
-        })
-        expect(useQueryMock).toHaveBeenCalledTimes(1)
-        expect(args.length).toBe(2)
-        expect(args[1]).toEqual(
-            expect.objectContaining({
-                enabled: false,
-            })
-        )
-    })
-    it('should not fetch offline units while configOfflineOrgUnitLevel are loading', () => {
-        const useQueryMock = jest.fn(() => ({ data: undefined }))
-        useQuery.mockImplementationOnce(useQueryMock)
-        useUserOrgUnits.mockReturnValueOnce({
-            loading: false,
-        })
-        useOrganisationUnitLevels.mockReturnValueOnce({
-            loading: false,
-        })
-        useLoadConfigOfflineOrgUnitLevel.mockReturnValueOnce({
-            loading: true,
-        })
-
-        const { result } = renderHook(usePrefetchedOrganisationUnits)
-        const args = useQueryMock.mock.calls[0]
-
-        expect(result.current).toEqual({
-            organisationUnits: undefined,
-            offlineLevels: undefined,
-            loading: true,
-            error: undefined,
-        })
-        expect(useQueryMock).toHaveBeenCalledTimes(1)
-        expect(args.length).toBe(2)
-        expect(args[1]).toEqual(
-            expect.objectContaining({
-                enabled: false,
-            })
-        )
-    })
-    it('should fetch offline units when the dependent queries are done loading', () => {
-        const useQueryMock = jest.fn(() => ({ data: undefined }))
-        useQuery.mockImplementationOnce(useQueryMock)
-        useUserOrgUnits.mockReturnValueOnce({
-            loading: false,
-            data: fixtures.userOrgUnits,
-        })
-        useOrganisationUnitLevels.mockReturnValueOnce({
-            loading: false,
-            data: fixtures.organisationUnitLevels,
-        })
-        useLoadConfigOfflineOrgUnitLevel.mockReturnValueOnce({
-            loading: false,
-            data: fixtures.configOfflineOrgUnitLevel,
-        })
-
-        const { result } = renderHook(usePrefetchedOrganisationUnits)
-        const args = useQueryMock.mock.calls[0]
-        const options = args[1]
-        const params = args[0][1].params
-
-        expect(result.current).toEqual({
-            organisationUnits: undefined,
-            offlineLevels: 3,
+    it('should fetch offline units when the offline levels are available', () => {
+        const offlineLevels = {
+            rootId1: [1, 2, 3],
+            rootId2: [2, 3],
+        }
+        const useOfflineLevelsMock = jest.fn(() => ({
+            offlineLevels,
             loading: false,
             error: undefined,
+        }))
+        useOfflineLevels.mockImplementationOnce(useOfflineLevelsMock)
+        const useQueriesMock = jest.fn(() => [{ loading: true, data: {} }])
+        useQueries.mockImplementationOnce(useQueriesMock)
+
+        const { result } = renderHook(usePrefetchedOrganisationUnits)
+
+        const args = useQueriesMock.mock.calls[0]
+        const arg = args[0]
+
+        expect(result.current).toEqual({
+            offlineLevels,
+            loading: false,
+            error: undefined,
+            offlineOrganisationUnits: [{}],
         })
-        expect(useQueryMock).toHaveBeenCalledTimes(1)
-        expect(args.length).toBe(2)
-        expect(options).toEqual(
-            expect.objectContaining({
-                enabled: true,
-            })
-        )
-        expect(params).toEqual({
-            fields: ['id', 'displayName', 'path', 'children::size', 'level'],
-            paging: false,
-            filter: 'level:in:[1,2,3]',
+        expect(useQueriesMock).toHaveBeenCalledTimes(1)
+        expect(args.length).toBe(1)
+        expect(arg).toEqual({
+            queries: [
+                {
+                    enabled: true,
+                    select: expect.any(Function),
+                    queryKey: [
+                        'organisationUnits',
+                        {
+                            id: 'rootId1',
+                            params: {
+                                fields: [
+                                    'id',
+                                    'displayName',
+                                    'path',
+                                    'children::size',
+                                    'level',
+                                ],
+                                filter: 'level:in:[1,2,3]',
+                                includeDescendants: true,
+                                paging: false,
+                            },
+                        },
+                    ],
+                },
+                {
+                    enabled: true,
+                    select: expect.any(Function),
+                    queryKey: [
+                        'organisationUnits',
+                        {
+                            id: 'rootId2',
+                            params: {
+                                fields: [
+                                    'id',
+                                    'displayName',
+                                    'path',
+                                    'children::size',
+                                    'level',
+                                ],
+                                filter: 'level:in:[2,3]',
+                                includeDescendants: true,
+                                paging: false,
+                            },
+                        },
+                    ],
+                },
+            ],
         })
     })
 })
