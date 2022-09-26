139c159a1105ee2b21c8818e1afd405c94886ddf
Hendrik de Graaf <hendrik@dhis2.org>
2022-09-26 16:45:56 +0200

    fix: use prefetched units when user org unit is not the highest level

--- src/context-selection/org-unit-selector-bar-item/organisation-unit-tree/helpers/get-pre-fetched-children.js
+++ src/context-selection/org-unit-selector-bar-item/organisation-unit-tree/helpers/get-pre-fetched-children.js
@@ -1,3 +1,22 @@
+/*
+ * A note about the current implementation:
+ * We start with the user-org-units, which are fechted from the `/me`
+ * endpoint. From this endpoint the `path` doesn't go all the way up
+ * to the real org-unit root, but to the highest user org unit.
+ * Then we pre-fetch a few levels of children for each user root
+ * and for this we use the `/organisationUnits` endpoint. From this
+ * endpoint the `path` that is returned goes all the way up to the
+ * real org-unit root. So there is a mismatch in path definitions
+ * between the two endpoints. As a result the same org-unit-could be
+ * identified by 2 different paths, i.e. a/b/c/d and b/c/d are in fact
+ * the same org-units. We can deal with this by shortening *all* paths
+ * so they always start at the current parent's ID.
+ */
+
+const getSubPathStartingWithId = (path, id) =>
+    // get substring starting with id and trim leading and trailing slashes
+    path.substring(path.indexOf(id)).replace(/^\/|\/$/g, '')
+
 export const getPreFetchedChildren = (offlineUnits, parent) => {
     if (!offlineUnits) {
         throw new Error('No offline organisation units available')
@@ -6,16 +25,13 @@ export const getPreFetchedChildren = (offlineUnits, parent) => {
         throw new Error('No parent to fetch children from')
     }
 
-    const fullParentPath = `${parent.path}/`
+    const parentSubPath = getSubPathStartingWithId(parent.path, parent.id)
 
     return offlineUnits
-        .filter(({ id, path }) => {
-            const unitPathFromUserRoot = path.substring(path.indexOf(parent.id))
-            const fullUnitPath = `/${unitPathFromUserRoot}`
-            const isDescendant = fullUnitPath.startsWith(fullParentPath)
-            const isDirectDescendant =
-                fullUnitPath.replace(fullParentPath, '') === id
-            return isDescendant && isDirectDescendant
-        })
+        .filter(
+            ({ id, path }) =>
+                `${parentSubPath}/${id}` ===
+                getSubPathStartingWithId(path, parent.id)
+        )
         .sort((a, b) => a.displayName > b.displayName)
 }
