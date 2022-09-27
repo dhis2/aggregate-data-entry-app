2e396b9fc4e6c85341ecdf155884175e0fd6ac16
Thomas Zemp <tom@dhis2.org>
2022-09-27 13:03:04 +0200

    fix: work on fixing cypress tests

--- cypress/integration/context-selection/section-filter/index.js
+++ cypress/integration/context-selection/section-filter/index.js
@@ -34,7 +34,7 @@ Given(
     'a data set, org unit and period have been selected and the data set has a tabbed sectioned form',
     () => {
         cy.visit(
-            '/#/?dataSetId=V8MHeZHIrcP&orgUnitId=ImspTQPwCqd&periodId=2021'
+            '/#/?dataSetId=Rl58JxmKJo2&orgUnitId=ImspTQPwCqd&periodId=202112'
         )
         cy.get('[data-test="data-set-selector"]').should('exist')
     }
@@ -44,7 +44,7 @@ Given(
     'a data set, org unit and period have been selected and the data set has some sections',
     () => {
         cy.visit(
-            '/#/?dataSetId=Rl58JxmKJo2&orgUnitId=ImspTQPwCqd&periodId=2020'
+            '/#/?dataSetId=V8MHeZHIrcP&orgUnitId=O6uvpzGd5pu&periodId=2020'
         )
         cy.get('[data-test="data-set-selector"]').should('exist')
     }
