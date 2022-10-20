204a71a784e3f97df2cbb08b941d0a227d56a658
Birk Johansson <birk@dhis2.org>
2022-10-20 02:15:38 +0200

    refactor: fix variable naming

--- src/shared/api-errors/api-mutation-error.js
+++ src/shared/api-errors/api-mutation-error.js
@@ -3,7 +3,7 @@ import i18n from '../../locales/index.js'
 /* A dictionary taking the shape Dictionary<ErrorCode, (string | (error) => string)> to override the display message for a certain error code
  * Potential error codes can be found here: https://github.com/dhis2/dhis2-core/blob/28a6aa052f221626e583a84bc18d80c0a8fa0927/dhis-2/dhis-api/src/main/java/org/hisp/dhis/feedback/ErrorCode.java#L97
  */
-const customErrorAlertProps = {
+const customErrorMessages = {
     // E2017: i18n.t('Here we could override the message for E2017 error with a custom message in the FE')
     E2022: i18n.t(`Selected period is after latest open future period.`),
 }
@@ -11,21 +11,21 @@ const customErrorAlertProps = {
 /**
  * A dictionary taking the shape Dictionary<HttpStatusCode, (string | (error) => string) >
  */
-const statusCodeErrorAlertProps = {
+const statusCodeErrorMessages = {
     // 400:  i18n.t(
     //         'Session Expired. Please reload to log in again. You will not lose any data.'
     //      ),
 }
 
 /**
- * This is a list over maps from error.details.key to the customErrorAlertProps-map above.
+ * This is a list over maps from error.details.key to the customErrorMessages-map above.
  * Array is used to preserve the order.
  *  Eg if error has both .errorCode and .httpStatusCode, errorCode will be used to get the custom-message.
  */
 
 const errorDetailsMessagePriority = [
-    ['errorCode', customErrorAlertProps],
-    ['httpStatusCode', statusCodeErrorAlertProps],
+    ['errorCode', customErrorMessages],
+    ['httpStatusCode', statusCodeErrorMessages],
 ]
 
 // FetchError is not exposed from app-runtime
