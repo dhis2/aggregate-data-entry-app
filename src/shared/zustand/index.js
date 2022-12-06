23d8182d5668915c54fc77afaf773455b2206528
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-12-06 10:10:50 +0800

    feat(zustand): create wrapper

---
+++ src/shared/zustand/index.js
@@ -0,0 +1,18 @@
+import createOriginal from 'zustand'
+import * as zustand from 'zustand'
+
+const createStateWrapper = createState => (set, get) => {
+    const config = createState(set, get)
+    return { setState: set, ...config }
+}
+
+// when creating a store, we get its initial state, create a reset function and add it in the set
+export default function create(createState) {
+    return createOriginal(createStateWrapper(createState))
+}
+
+export function createStore(createState) {
+    return zustand.createStore(createStateWrapper(createState))
+}
+
+export { useStore } from 'zustand'
