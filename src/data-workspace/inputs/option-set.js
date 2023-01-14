8ae511c830c3535465f1c12b28944602059b28c2
Birk Johansson <birk@dhis2.org>
2023-01-14 19:16:32 +0100

    feat(optionset): add support for multi-text optionSet

--- src/data-workspace/inputs/option-set.js
+++ src/data-workspace/inputs/option-set.js
@@ -1,5 +1,11 @@
 import i18n from '@dhis2/d2-i18n'
-import { Button, SingleSelect, SingleSelectOption } from '@dhis2/ui'
+import {
+    Button,
+    SingleSelect,
+    MultiSelect,
+    MultiSelectOption,
+    SingleSelectOption,
+} from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { useField } from 'react-final-form'
@@ -21,21 +27,43 @@ export const OptionSet = ({
     onFocus,
     disabled,
     locked,
+    multi,
 }) => {
+    const parse = (value) => {
+        console.log('parse', value)
+        if (multi) {
+            return (value && value.join(',')) || ''
+        } else {
+            // Empty values need an empty string
+            return value || ''
+        }
+    }
+
     const {
         input,
         meta: { data },
     } = useField(fieldname, {
         subscription: { value: true, data: true },
+        format:
+            // format to an array when multi, since component needs an array
+            multi
+                ? (value) => {
+                      console.log({ value })
+                      const formatted = (value && value.split(',')) || []
+                      return formatted
+                  }
+                : undefined,
+        // parse from array to a string, since the api expects a string
+        parse: multi ? parse : undefined,
     })
+
     const { data: metadata } = useMetadata()
 
     const { mutate } = useSetDataValueMutation({ deId, cocId })
     const syncData = (value) => {
         // todo: Here's where an error state could be set: ('onError')
         mutate(
-            // Empty values need an empty string
-            { value: value || '' },
+            { value },
             {
                 onSuccess: () => {
                     form.mutators.setFieldData(fieldname, {
@@ -49,7 +77,9 @@ export const OptionSet = ({
     const handleChange = (value) => {
         // For a select using onChange, don't need to check valid or dirty, respectively
         if (value !== data.lastSyncedValue) {
-            syncData(value)
+            // need to parse here as well, since it's not parsed before onChange is called
+            const parsedValue = parse(value)
+            syncData(parsedValue)
         }
     }
 
@@ -57,16 +87,23 @@ export const OptionSet = ({
     // filter out 'null' options
     const options = optionSet.options.filter((opt) => !!opt)
 
+    const SelectComponent = multi ? MultiSelect : SingleSelect
+    const SelectOptionComponent = multi ? MultiSelectOption : SingleSelectOption
+    const placeholder = multi
+        ? i18n.t('Choose option(s)')
+        : i18n.t('Choose an option')
+    // if(multi) {
+    //     return <MultiSelectOptionSet options={options} input={input} disabled={disabled || locked} />
+    // }
     // todo: onBlur handler doesn't work, meaning the cell stays active.
-    // may need to build from scratch
     return (
         <div className={styles.selectFlexWrapper} onClick={onFocus}>
             <div className={styles.selectFlexItem}>
-                <SingleSelect
+                <SelectComponent
                     dense
                     className={styles.select}
                     name={input.name}
-                    placeholder={i18n.t('Choose an option')}
+                    placeholder={placeholder}
                     selected={input.value || ''}
                     onChange={({ selected }) => {
                         input.onChange(selected)
@@ -81,22 +118,22 @@ export const OptionSet = ({
                     onKeyDown={onKeyDown}
                     onBlur={() => input.onBlur()}
                     disabled={disabled || locked}
+                    inputMaxHeight={'24px'}
                 >
                     {options.map(({ id, code, displayName }) => (
-                        <SingleSelectOption
+                        <SelectOptionComponent
                             key={id}
                             label={displayName}
                             value={code}
                         />
                     ))}
-                </SingleSelect>
+                </SelectComponent>
             </div>
             {input.value && (
                 <Button
                     small
                     secondary
                     className={styles.whiteButton}
-                    {...input}
                     onClick={() => {
                         input.onChange('')
                         handleChange('')
@@ -112,5 +149,6 @@ export const OptionSet = ({
 }
 OptionSet.propTypes = {
     ...InputPropTypes,
+    multi: PropTypes.bool,
     optionSetId: PropTypes.string,
 }
