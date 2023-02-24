cbd4958bb706eea4e0101de51a5891f117037947
Kai Vandivier <kpvandivier@gmail.com>
2023-02-24 19:31:35 +0100

    fix: adjust for different time locales

--- src/bottom-bar/form-expiry-info.js
+++ src/bottom-bar/form-expiry-info.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import i18n from '../locales/index.js'
-import { useLockedContext, usePeriod, usePeriodId } from '../shared/index.js'
+import { useLockedContext } from '../shared/index.js'
 
 export default function FormExpiryInfo() {
-    const [periodId] = usePeriodId()
-    const selectedPeriod = usePeriod(periodId)
-    console.log({ selectedPeriod })
     const {
         locked,
         lockStatus: { lockDate },
     } = useLockedContext()
 
-    console.log({ locked, lockDate })
-
     return (
         <>
             {!locked && lockDate && (
                 <div>
-                    {i18n.t('This form closes at {{dateTime}}', {
-                        dateTime: lockDate.toUTCString(),
+                    {i18n.t('This form closes at {{-dateTime}}', {
+                        dateTime: lockDate.toLocaleString(),
                     })}
                 </div>
             )}
--- src/shared/locked-status/use-check-lock-status.js
+++ src/shared/locked-status/use-check-lock-status.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
-import { formatJsDateToDateString, useClientServerDate } from '../date/index.js'
+import { useClientServerDateUtils } from '../date/index.js'
+import { getCurrentDate } from '../fixed-periods/index.js'
 import { useMetadata, selectors } from '../metadata/index.js'
 import usePeriod from '../period/use-period.js'
 import {
@@ -11,18 +12,19 @@ import { useDataValueSet } from '../use-data-value-set/use-data-value-set.js'
 import { LockedStates, BackendLockStatusMap } from './locked-states.js'
 import { useLockedContext } from './use-locked-context.js'
 
+const DAY_MS = 24 * 60 * 60 * 1000
+
 /** Check for status relative to dataInputPeriods and expiryDays */
 const getFrontendLockStatus = ({
     dataSetId,
-    periodId,
     metadata,
-    currentDayString,
     selectedPeriod,
+    clientServerDateUtils: { fromServerDate, fromClientDate },
 }) => {
     const applicableDataInputPeriod = selectors.getApplicableDataInputPeriod(
         metadata,
         dataSetId,
-        periodId
+        selectedPeriod.id
     )
     const expiryDays = selectors.getDataSetById(metadata, dataSetId)?.expiryDays
 
@@ -31,18 +33,23 @@ const getFrontendLockStatus = ({
         return null
     }
 
-    let lockDate
-    const currentDateAtServerTimeZone = new Date(currentDayString)
+    let clientLockDate
+    const currentDate = fromClientDate(getCurrentDate())
 
     if (applicableDataInputPeriod) {
         const { openingDate, closingDate } = applicableDataInputPeriod
-        // openingDate and closingDate can be undefined
-        const parsedOpeningDate = openingDate && new Date(openingDate)
-        const parsedClosingDate = closingDate && new Date(closingDate)
+        // openingDate and closingDate can be undefined.
+        // They are local to the server
+        const parsedOpeningDate =
+            openingDate && fromServerDate(new Date(openingDate))
+        const parsedClosingDate =
+            closingDate && fromServerDate(new Date(closingDate))
 
         if (
-            (openingDate && currentDateAtServerTimeZone < parsedOpeningDate) ||
-            (closingDate && currentDateAtServerTimeZone > parsedClosingDate)
+            (openingDate &&
+                currentDate.serverDate < parsedOpeningDate.serverDate) ||
+            (closingDate &&
+                currentDate.serverDate > parsedClosingDate.serverDate)
         ) {
             return {
                 state: LockedStates.LOCKED_DATA_INPUT_PERIOD,
@@ -50,29 +57,35 @@ const getFrontendLockStatus = ({
             }
         } else {
             // This might still be undefined, but that's okay
-            lockDate = parsedClosingDate
+            clientLockDate = parsedClosingDate.clientDate
         }
     }
 
     if (expiryDays > 0) {
-        const hourMs = 60 * 60 * 1000
-        const dayMs = hourMs * 24
-        // Do operations in ms
-        const expiryDate = new Date(
-            new Date(selectedPeriod.endDate).getTime() +
-                // Add one day pecause selectedPeriod.endDate is 00:00 of the last
-                // day of the period (i.e. 24 hours before when the period ends),
-                // and we want midnight of the day it expires
-                (expiryDays + 1) * dayMs
+        // selectedPeriod.endDate is a string like '2023-02-20' --
+        // convert that to a date and get time to get ms to add expiry days.
+        // Also add one day more because selectedPeriod.endDate is the START
+        // of the period's last day (00:00), and we want the end of that day
+        // (confirmed with backend behavior).
+        // Converting the selectedPeriod.endDate string to an object creates
+        // it in the client's timezone; parse it from there.
+        const expiryDate = fromClientDate(
+            new Date(
+                new Date(selectedPeriod.endDate).getTime() +
+                    (expiryDays + 1) * DAY_MS
+            )
         )
 
-        if (currentDateAtServerTimeZone < expiryDate) {
+        if (currentDate.serverDate < expiryDate.serverDate) {
             // Take the sooner of the two possible lock dates
-            lockDate = lockDate
-                ? new Date(Math.min(lockDate, expiryDate))
-                : expiryDate
-            // This value might be misleading if a superuser or lock exception
-            // would override it; it would take checking those on the front end
+            clientLockDate = clientLockDate
+                ? new Date(Math.min(clientLockDate, expiryDate.clientDate))
+                : expiryDate.clientDate
+            // ! NB:
+            // This value is still shown, even if a form won't actually lock
+            // for a user due to superuser or lock exceptions, which might be
+            // misleading. That would take checking those exceptions on the
+            // front end, which are currently done on the backend
         }
 
         // If this form is actually expired, don't lock it here; leave that
@@ -80,29 +93,28 @@ const getFrontendLockStatus = ({
         // lock exceptions
     }
 
-    return { state: LockedStates.OPEN, lockDate }
+    return { state: LockedStates.OPEN, lockDate: clientLockDate }
 }
 
 export const useCheckLockStatus = () => {
     const [dataSetId] = useDataSetId()
     const [orgUnitId] = useOrgUnitId()
     const [periodId] = usePeriodId()
-    const currentDate = useClientServerDate()
-    const currentDayString = formatJsDateToDateString(currentDate.serverDate)
-    const { data: metadata } = useMetadata()
-    const { setLockStatus } = useLockedContext()
-    const dataValueSet = useDataValueSet()
     const selectedPeriod = usePeriod(periodId)
+
+    const clientServerDateUtils = useClientServerDateUtils()
+
+    const { data: metadata } = useMetadata()
+    const dataValueSet = useDataValueSet()
+    const { setLockStatus } = useLockedContext()
 
     useEffect(() => {
         const frontendLockStatus = getFrontendLockStatus({
             dataSetId,
-            periodId,
-            metadata,
-            currentDayString,
             selectedPeriod,
+            metadata,
+            clientServerDateUtils,
         })
-        console.log({ frontendLockStatus })
         if (frontendLockStatus) {
             setLockStatus(frontendLockStatus)
             return
@@ -122,10 +134,9 @@ export const useCheckLockStatus = () => {
         metadata,
         dataSetId,
         orgUnitId,
-        periodId,
+        clientServerDateUtils,
         dataValueSet.data?.lockStatus,
         setLockStatus,
-        currentDayString,
         selectedPeriod,
     ])
 }
