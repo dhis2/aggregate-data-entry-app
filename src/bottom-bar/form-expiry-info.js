7f7d429284f8b799f073fab14db9e54c19fa63a9
Kai Vandivier <kpvandivier@gmail.com>
2023-02-25 15:24:46 +0100

    feat: nice UI

--- src/bottom-bar/form-expiry-info.js
+++ src/bottom-bar/form-expiry-info.js
@@ -1,6 +1,9 @@
+import { IconInfo16, colors } from '@dhis2/ui'
+import cx from 'classnames'
 import React from 'react'
 import i18n from '../locales/index.js'
 import { useLockedContext } from '../shared/index.js'
+import styles from './main-tool-bar.module.css'
 
 export default function FormExpiryInfo() {
     const {
@@ -11,11 +14,19 @@ export default function FormExpiryInfo() {
     return (
         <>
             {!locked && lockDate && (
-                <div>
-                    {i18n.t('This form closes at {{-dateTime}}', {
-                        dateTime: lockDate.toLocaleString(),
-                    })}
-                </div>
+                <span
+                    className={cx(styles.completionSummary, styles.toolbarItem)}
+                >
+                    <span className={cx(styles.icon)}>
+                        <IconInfo16 color={colors.grey700} />
+                    </span>
+
+                    <span>
+                        {i18n.t('This form closes at {{-dateTime}}', {
+                            dateTime: lockDate.toLocaleString(),
+                        })}
+                    </span>
+                </span>
             )}
         </>
     )
