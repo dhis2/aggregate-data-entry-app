0bca4486060d0794f4c9a849d9122fa0307622f1
Adel Bensaad <adel@dhis2.org>
2023-08-01 15:28:54 +0100

    test: publish cypress test results to reportportal

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const {
     chromeAllowXSiteCookies,
     cucumberPreprocessor,
 } = require('@dhis2/cypress-plugins')
+const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin')
 const { defineConfig } = require('cypress')
 
 // This function will be modified when we add plugins
@@ -10,6 +11,7 @@ async function setupNodeEvents(on, config) {
     await cucumberPreprocessor(on, config)
     networkShim(on, config)
     chromeAllowXSiteCookies(on, config)
+    registerReportPortalPlugin(on, config)
     return config
 }
 
@@ -18,6 +20,23 @@ module.exports = defineConfig({
     projectId: 's6p5xs',
     experimentalInteractiveRunEvents: true,
     defaultCommandTimeout: 30000,
+    reporter: '@reportportal/agent-js-cypress',
+    reporterOptions: {
+        endpoint: 'https://test.tools.dhis2.org/reportportal/api/v1',
+        apiKey: process.env.CYPRESS_RP_API_KEY,
+        launch: 'aggregate_data_entry_app_master',
+        project: 'dhis2_auto',
+        description: '',
+        attributes: [
+            {
+                key: 'version',
+                value: 'master',
+            },
+            {
+                value: 'aggregate_data_entry_app',
+            },
+        ],
+    },
     e2e: {
         setupNodeEvents,
         baseUrl: 'http://localhost:3000',
