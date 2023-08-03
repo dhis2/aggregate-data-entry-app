41ee8508fe6962aaab95ca48c54fe2401970ef4c
Adel Bensaad <adel@dhis2.org>
2023-08-03 15:24:46 +0100

    test: publish jest test results to reportportal

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,33 @@ module.exports = {
     moduleNameMapper: {
         '\\.css$': 'identity-obj-proxy',
     },
+    testRunner: 'jest-circus/runner',
+    reporters: [
+        'default',
+        [
+            '@reportportal/agent-js-jest',
+            {
+                apiKey: process.env.REPORTPORTAL_API_KEY,
+                endpoint: 'https://test.tools.dhis2.org/reportportal/api/v1',
+                project: 'dhis2_auto',
+                launch: 'aggregate_data_entry_app_master',
+                attributes: [
+                    {
+                        key: 'version',
+                        value: 'master',
+                    },
+                    {
+                        key: 'app_name',
+                        value: 'aggregate_data_entry_app',
+                    },
+                    {
+                        key: 'test_level',
+                        value: 'unit/integration',
+                    },
+                ],
+                description: '',
+                debug: true,
+            },
+        ],
+    ],
 }
