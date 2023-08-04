418bf924cb099fcee7f13e859ccb6da4ea1ca54f
Adel Bensaad <adel@dhis2.org>
2023-08-04 10:54:26 +0100

    test: publish cypress test results to reportportal (#349)
    * test: publish cypress test results to reportportal
    
    * test: update API_KEY name and merge parallel launches
    
    * test: debug merging launches
    
    * test: cleaning PR
    
    * test: use import instead of require

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const {
     chromeAllowXSiteCookies,
     cucumberPreprocessor,
 } = require('@dhis2/cypress-plugins')
+const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin')
 const { defineConfig } = require('cypress')
 
 // This function will be modified when we add plugins
@@ -10,6 +11,7 @@ async function setupNodeEvents(on, config) {
     await cucumberPreprocessor(on, config)
     networkShim(on, config)
     chromeAllowXSiteCookies(on, config)
+    registerReportPortalPlugin(on, config)
     return config
 }
 
@@ -18,6 +20,30 @@ module.exports = defineConfig({
     projectId: 's6p5xs',
     experimentalInteractiveRunEvents: true,
     defaultCommandTimeout: 30000,
+    reporter: '@reportportal/agent-js-cypress',
+    reporterOptions: {
+        endpoint: 'https://test.tools.dhis2.org/reportportal/api/v1',
+        apiKey: process.env.REPORTPORTAL_API_KEY,
+        launch: 'aggregate_data_entry_app_master',
+        project: 'dhis2_auto',
+        description: '',
+        autoMerge: true,
+        parallel: true,
+        debug: false,
+        restClientConfig: {
+            timeout: 360000,
+        },
+        attributes: [
+            {
+                key: 'version',
+                value: 'master',
+            },
+            {
+                key: 'app_name',
+                value: 'aggregate_data_entry_app',
+            },
+        ],
+    },
     e2e: {
         setupNodeEvents,
         baseUrl: 'http://localhost:3000',
