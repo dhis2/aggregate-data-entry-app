25070eba115aaa791269a5b8add2c191f7eda741
Birk Johansson <birk@dhis2.org>
2023-12-11 14:03:56 +0100

    fix(tests): update tests

--- src/data-workspace/inputs/validators.test.js
+++ src/data-workspace/inputs/validators.test.js
@@ -1,5 +1,8 @@
 import { VALUE_TYPES, CAN_HAVE_LIMITS_TYPES } from '../../shared/index.js'
-import { validateByValueType } from './validators.js'
+import {
+    validateByValueType,
+    warningValidateByValueType,
+} from './validators.js'
 
 describe(`validateByValueTypeWithLimits`, () => {
     describe(`when limits are defined`, () => {
@@ -12,8 +15,6 @@ describe(`validateByValueTypeWithLimits`, () => {
                     value: 4,
                     min: 5,
                     max: 10,
-                    expectedErrorMessage:
-                        'Number cannot be less than 5 or more than 10',
                 },
             ]),
             [
@@ -22,18 +23,17 @@ describe(`validateByValueTypeWithLimits`, () => {
                     value: -4,
                     min: -10,
                     max: -5,
-                    expectedErrorMessage:
-                        'Number cannot be less than -10 or more than -5',
                 },
             ],
         ])(
-            'should return error for %s type that supports limits where value is outside of bounds',
-            ({ valueType, value, min, max, expectedErrorMessage }) => {
+            'should not return error for %s type that supports limits where value is outside of bounds',
+            ({ valueType, value, min, max }) => {
+                // limits are handled as warnings - not errors!
                 const errorMessage = validateByValueType(valueType, {
                     min,
                     max,
                 })(value)
-                expect(errorMessage).toEqual(expectedErrorMessage)
+                expect(errorMessage).toEqual(undefined)
             }
         )
 
@@ -74,6 +74,98 @@ describe(`validateByValueTypeWithLimits`, () => {
         it('should not return error for type that support limits and only has min defined', () => {
             expect(
                 validateByValueType(VALUE_TYPES.NUMBER, {
+                    max: 10,
+                })(7)
+            ).toBeUndefined()
+        })
+
+        it('should return error when valueType is number and value is not a number', () => {
+            expect(validateByValueType(VALUE_TYPES.NUMBER, {})('asdf')).toEqual(
+                'Please provide a number'
+            )
+        })
+
+        it('should return error when not a valid percentage', () => {
+            expect(
+                validateByValueType(VALUE_TYPES.PERCENTAGE, {})(101)
+            ).toEqual('Number cannot be less than 0 or more than 100')
+        })
+    })
+})
+
+describe('warningValidateByValueType', () => {
+    describe(`when limits are defined`, () => {
+        it.each([
+            ...CAN_HAVE_LIMITS_TYPES.filter(
+                (valueType) => valueType !== VALUE_TYPES.INTEGER_NEGATIVE
+            ).map((valueType) => [
+                {
+                    valueType,
+                    value: 4,
+                    min: 5,
+                    max: 10,
+                    expectedErrorMessage:
+                        'Number cannot be less than 5 or more than 10',
+                },
+            ]),
+            [
+                {
+                    valueType: VALUE_TYPES.INTEGER_NEGATIVE,
+                    value: -4,
+                    min: -10,
+                    max: -5,
+                    expectedErrorMessage:
+                        'Number cannot be less than -10 or more than -5',
+                },
+            ],
+        ])(
+            'should return error for %s type that supports limits where value is outside of bounds',
+            ({ valueType, value, min, max, expectedErrorMessage }) => {
+                const errorMessage = warningValidateByValueType(valueType, {
+                    min,
+                    max,
+                })(value)
+                expect(errorMessage).toEqual(expectedErrorMessage)
+            }
+        )
+
+        it('should not return error for type that support limits and is in bounds', () => {
+            expect(
+                warningValidateByValueType(VALUE_TYPES.NUMBER, {
+                    min: 5,
+                    max: 10,
+                })(7)
+            ).toBeUndefined()
+        })
+
+        it('should not return error for type that does not support limits', () => {
+            expect(
+                warningValidateByValueType(VALUE_TYPES.TEXT, {
+                    min: 5,
+                    max: 10,
+                })(`asdf`)
+            ).toBeUndefined()
+        })
+    })
+
+    describe(`when limits are undefined`, () => {
+        it('should not return error', () => {
+            expect(
+                warningValidateByValueType(VALUE_TYPES.INTEGER, undefined)(4)
+            ).toBeUndefined()
+        })
+
+        it('should not return error for type that support limits and only has min defined', () => {
+            expect(
+                warningValidateByValueType(VALUE_TYPES.NUMBER, {
+                    min: 5,
+                })(7)
+            ).toBeUndefined()
+        })
+
+        it('should not return error for type that support limits and only has min defined', () => {
+            expect(
+                warningValidateByValueType(VALUE_TYPES.NUMBER, {
                     max: 10,
                 })(7)
             ).toBeUndefined()
