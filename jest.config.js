5bd53e7299303749c6e0115432f60137cdea5229
Birk Johansson <birkbjo@gmail.com>
2023-12-13 11:43:34 +0100

    chore(report-portal): run reports only when env is setup (#363)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,34 @@
+const reportPortalConfig = [
+    '@reportportal/agent-js-jest',
+    {
+        apiKey: process.env.REPORTPORTAL_API_KEY,
+        endpoint: process.env.REPORTPORTAL_ENDPOINT,
+        project: process.env.REPORTPORTAL_PROJECT,
+        launch: 'aggregate_data_entry_app_master',
+        attributes: [
+            {
+                key: 'version',
+                value: 'master',
+            },
+            {
+                key: 'app_name',
+                value: 'aggregate_data_entry_app',
+            },
+            {
+                key: 'test_level',
+                value: 'unit/integration',
+            },
+        ],
+        description: '',
+        debug: true,
+    },
+]
+
+const isReportPortalSetup =
+    process.env.REPORTPORTAL_API_KEY !== undefined &&
+    process.env.REPORTPORTAL_ENDPOINT !== undefined &&
+    process.env.REPORTPORTAL_PROJECT !== undefined
+
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/src/test-utils/setup-tests.js'],
     collectCoverageFrom: ['src/**/*.{js,jsx}'],
@@ -12,30 +43,6 @@ module.exports = {
     testRunner: 'jest-circus/runner',
     reporters: [
         'default',
-        [
-            '@reportportal/agent-js-jest',
-            {
-                apiKey: process.env.REPORTPORTAL_API_KEY,
-                endpoint: process.env.REPORTPORTAL_ENDPOINT,
-                project: process.env.REPORTPORTAL_PROJECT,
-                launch: 'aggregate_data_entry_app_master',
-                attributes: [
-                    {
-                        key: 'version',
-                        value: 'master',
-                    },
-                    {
-                        key: 'app_name',
-                        value: 'aggregate_data_entry_app',
-                    },
-                    {
-                        key: 'test_level',
-                        value: 'unit/integration',
-                    },
-                ],
-                description: '',
-                debug: true,
-            },
-        ],
+        ...(isReportPortalSetup ? [reportPortalConfig] : []),
     ],
 }
