1482aa6588f75ab5feb6a17ba5fbc2d6e222bde5
ismay <7355199+ismay@users.noreply.github.com>
2024-10-15 12:59:55 +0200

    ci: update workflows (#401)
    * ci: update workflows
    
    * ci: add netlify deploy workflows
    
    * ci: add e2e
    
    * ci: remove custom cypress reporter
    
    * chore: remove jest reporting

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const {
     chromeAllowXSiteCookies,
     cucumberPreprocessor,
 } = require('@dhis2/cypress-plugins')
-const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin')
 const { defineConfig } = require('cypress')
 
 // This function will be modified when we add plugins
@@ -11,7 +10,6 @@ async function setupNodeEvents(on, config) {
     await cucumberPreprocessor(on, config)
     networkShim(on, config)
     chromeAllowXSiteCookies(on, config)
-    registerReportPortalPlugin(on, config)
     return config
 }
 
@@ -20,34 +18,6 @@ module.exports = defineConfig({
     projectId: 's6p5xs',
     experimentalInteractiveRunEvents: true,
     defaultCommandTimeout: 30000,
-    reporter: '@reportportal/agent-js-cypress',
-    reporterOptions: {
-        endpoint: process.env.REPORTPORTAL_ENDPOINT,
-        apiKey: process.env.REPORTPORTAL_API_KEY,
-        launch: 'aggregate_data_entry_app_master',
-        project: process.env.REPORTPORTAL_PROJECT,
-        description: '',
-        autoMerge: true,
-        parallel: true,
-        debug: false,
-        restClientConfig: {
-            timeout: 360000,
-        },
-        attributes: [
-            {
-                key: 'version',
-                value: 'master',
-            },
-            {
-                key: 'app_name',
-                value: 'aggregate_data_entry_app',
-            },
-            {
-                key: 'test_level',
-                value: 'e2e',
-            },
-        ],
-    },
     e2e: {
         setupNodeEvents,
         baseUrl: 'http://localhost:3000',
