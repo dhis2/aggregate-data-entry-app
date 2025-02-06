ae3daee45420c180964521fa68e7c729823c57fd
Kai Vandivier <kpvandivier@gmail.com>
2025-02-06 18:30:11 +0100

    fix(app-platform): upgrade platform tools to use vite and react 18

--- src/test-utils/setup-tests.js
+++ src/test-utils/setup-tests.js
@@ -1,6 +1,6 @@
+import Adapter from '@cfaester/enzyme-adapter-react-18'
 import { configure } from '@testing-library/dom'
 import { configure as configureEnzyme } from 'enzyme'
-import Adapter from '@cfaester/enzyme-adapter-react-18'
 import '@testing-library/jest-dom'
 import 'fake-indexeddb/auto'
 import * as matchers from 'jest-extended'
