a8c0b3ea0468e53572c64942f54fda405f1e7658
Chisomchima <chimachisom360@gmail.com>
2025-02-18 10:48:21 +0100

    feat: update app title to data entry
    BREAKING CHANGE: updates app title to data entry

--- d2.config.js
+++ d2.config.js
@@ -2,7 +2,7 @@ const config = {
     id: '4a5b87dc-015c-47db-ae77-f2f42e3bbb5a',
     name: 'aggregate-data-entry',
     type: 'app',
-    title: 'Data Entry (Beta)',
+    title: 'Data Entry',
     minDHIS2Version: '2.39',
     coreApp: true,
     pwa: {
