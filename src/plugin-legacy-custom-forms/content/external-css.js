04d943f937bc1aef06e9e9eb3819bbdfce8c787b
Mozafar <mozafar@dhis2.org>
2026-03-10 16:51:54 +0000

    fix: add bootstrap CSS and JS dependencies (#526)
    * docs: add note about styling
    
    * fix: add bootstrap and dependencies

--- src/plugin-legacy-custom-forms/content/external-css.js
+++ src/plugin-legacy-custom-forms/content/external-css.js
@@ -1,8 +1,10 @@
 const files = [
     'javascripts/jQuery/ui/css/redmond/jquery-ui.css',
+    'css/light_blue.css',
     'css/widgets.css', // removed jquery overrides (makes everything super small)
-    'css/dhis-web-dataentry.css',
     'css/select2/select2.css',
+    'css/dhis-web-dataentry.css',
+    'css/bootstrap/bootstrap.min.css',
 ]
 
 export default files
